89a6ecca031695b37c746550b063f57597baf325
tomdx <thomasle0@outlook.com>
2021-07-05 15:03:13 +1000

    Implement listener

---
+++ Main.java
@@ -0,0 +1,28 @@
+import Facts.Fact;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BilLexer bilLexer = new BilLexer(CharStreams.fromFileName("samples/complex.bil"));
+
+        CommonTokenStream tokens = new CommonTokenStream(bilLexer);
+        BilParser parser = new BilParser(tokens);
+        parser.setBuildParseTree(true);
+        BilParser.BilContext b = parser.bil();
+        JavaBilListener listener = new JavaBilListener();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(listener, b);
+
+        for (Fact fact : listener.facts) {
+            System.out.println(fact);
+        }
+    }
+}
