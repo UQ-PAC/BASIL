4cf6942ae652f5add6cea684eabea11c16b1b195
tomdx <thomasle0@outlook.com>
2021-07-06 09:40:33 +1000

    Remove program counter

--- Facts/SuccessorFact.java
+++ Facts/SuccessorFact.java
@@ -3,14 +3,13 @@ package Facts;
 public class SuccessorFact extends Fact {
     private String i1;
     private String i2;
-    public SuccessorFact(String pc, String i1, String i2) {
-        super(pc);
+    public SuccessorFact(String i1, String i2) {
         this.i1 = i1;
         this.i2 = i2;
     }
 
     @Override
     public String toString() {
-        return String.format("succ(%s,%s,%s)", this.pc, i1, i2);
+        return String.format("succ(%s,%s)", i1, i2);
     }
 }
