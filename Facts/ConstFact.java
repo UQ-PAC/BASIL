85d503c36776cc13129e4cfefe4506cd9614f092
tomdx <thomasle0@outlook.com>
2021-07-06 09:40:27 +1000

    Remove program counter

--- Facts/ConstFact.java
+++ Facts/ConstFact.java
@@ -3,15 +3,16 @@ package Facts;
 public class ConstFact extends Fact{
     public String var;
     public String literal;
+    public String pc;
 
     public ConstFact(String pc, String var, String literal) {
-        super(pc);
+        this.pc = pc;
         this.var = var;
         this.literal = literal;
     }
 
     public String toString() {
-        return String.format("const(%s,%s,%s)", super.pc, var, literal);
+        return String.format("const(%s,%s,%s)", pc, var, literal);
     }
 
 }
