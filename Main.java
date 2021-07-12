8e9a225a53cd45ae900caf9e330ea2c04ba7cc10
tomdx <thomasle0@outlook.com>
2021-07-12 15:03:39 +1000

    Refactor to work with different facts

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
