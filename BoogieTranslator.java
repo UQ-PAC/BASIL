619f2bda02aef8285090787933a18205862b0896
James Tobler <jamesjtobler@gmail.com>
2021-08-31 16:27:01 +1000

    refactored translator and added documentation

--- BoogieTranslator.java
+++ BoogieTranslator.java
@@ -6,12 +6,16 @@ import java.util.*;
 
 public class BoogieTranslator {
 
+    // for writing the boogie output
     BufferedWriter writer;
+    // the lines in the BIL file to translate
     List<ParserRuleContext> lines;
+    // the name of the function we are currently in, or "" if we are not in a function
     String funcName = "";
+    // the number of expected ParamType lines remaining after a Sub line is read
     int expectedParams = 0;
+    // line addresses that are used (i.e. jumped to) in the BIL program
     Set<String> usedLabels = new HashSet<>();
-    Map<String, Integer> registerValues = new HashMap<>();
     // the current index of 'lines' that is being analysed
     int lineIndex = 0;
 
@@ -32,9 +36,10 @@ public class BoogieTranslator {
         }
     }
 
+    /**
+     * Searches through the code and adds all used labels to the global usedLabels set
+     */
     private void logUsedLabels() {
-        // label analysis
-        // find all used labels
         for (ParserRuleContext line : lines) {
             String target = "";
             // labels are used in jumps, cjumps and calls
@@ -42,6 +47,7 @@ public class BoogieTranslator {
                 BilParser.StmtContext stmt = (BilParser.StmtContext) line;
                 if (stmt.jmp() != null) {
                     BilParser.JmpContext jump = stmt.jmp();
+                    // tom included these conditionals and i'm not sure why, but they're staying for now
                     if (jump.var() != null) {
                         target = jump.var().getText();
                     } else if (jump.addr() != null) {
@@ -61,6 +67,10 @@ public class BoogieTranslator {
         }
     }
 
+    /**
+     * Represents what to write at the top of the boogie file, when beginning the translation.
+     * Will usually include a bunch of variable initialisation, as per usual in boogie.
+     */
     private void handleInit() {
         // registers[0] represents the value at X0
         writeToFile("const registers: [int] int;\n");
@@ -74,6 +84,10 @@ public class BoogieTranslator {
         writeToFile("\n");
     }
 
+    /**
+     * Handles a line of BIL.
+     * @param line the line to handle.
+     */
     private void handleLine(ParserRuleContext line) {
         // lines can be statements, functions declarations (subs) functions returns (end subs) or function parameters
         if (line.getClass() == BilParser.StmtContext.class) {
@@ -92,6 +106,10 @@ public class BoogieTranslator {
         lineIndex++;
     }
 
+    /**
+     * Handles a statement, such as an assignment, jump, conditional jump or function call.
+     * @param ctx the statement to handle.
+     */
     private void handleStatement(BilParser.StmtContext ctx) {
         // visual index for functions
         if (!funcName.equals("")) {
@@ -117,6 +135,10 @@ public class BoogieTranslator {
         writeToFile(";");
     }
 
+    /**
+     * Handles an assignment, such as a load, store or move.
+     * @param ctx the assignment to handle.
+     */
     private void handleAssignment(BilParser.AssignContext ctx) {
         // assignments can be loads, stores or moves
         if (ctx.exp().getClass().equals(BilParser.ExpLoadContext.class)) {
@@ -128,6 +150,11 @@ public class BoogieTranslator {
         }
     }
 
+    /**
+     * Handles a load assignment.
+     * fixme: Incomplete.
+     * @param ctx the load to handle.
+     */
     private void handleLoad(BilParser.ExpLoadContext ctx) {
         // 'mem[x0,el]:u32'
         if (ctx.exp(2) == null) {
@@ -140,13 +167,27 @@ public class BoogieTranslator {
         writeToFile(String.format("%s := mem[%s]", lhs, rhs));
     }
 
+    /**
+     * Handles a store assignment.
+     * @param ctx the store to handle.
+     */
     private void handleStore(BilParser.ExpStoreContext ctx) {
         // 'memwith[X0,el]:u32<-low:32[X1]'
-        String lhs = parseExpression(ctx.exp(1)); // 'X0'
+        Integer lhs = evaluateExpression(ctx.exp(1), lineIndex);
+        String lhsStr;
+        if (lhs == null) {
+            lhsStr = parseExpression(ctx.exp(1));
+        } else {
+            lhsStr = String.valueOf(lhs);
+        }
         String rhs = parseExpression(ctx.exp(2)); // 'X1', potentially removes casts such as 'low:32[X1]'
-        writeToFile(String.format("mem[%s] := %s", evaluateExpression(ctx.exp(1), lineIndex), rhs)); // fixme
+        writeToFile(String.format("mem[%s] := %s", lhsStr, rhs));
     }
 
+    /**
+     * Handles a move assignment.
+     * @param ctx the move to handle.
+     */
     private void handleMove(BilParser.AssignContext ctx) {
         String lhs = ctx.var().getText();
         if (lhs.charAt(0) == 'X') {
@@ -157,16 +198,22 @@ public class BoogieTranslator {
     }
 
     /*
+    Returns the integer value of an expression, using backtracking to dereference variables.
+
+    How this works:
     We have an expression a + b.
-    Call evaluate expression(a + b).
-    This returns evaluate expression(a) + evaluate expression(b).
-    Evaluate expression(a) attempts to resolve a by searching backwards through the code.
-    It increments back until it finds a non-statement, or an assignment statement with lhs === a.
-    Once it finds this assignment statement, it calls evaluate expression on the rhs of the statement.
-    Global line counters aren't touched, by local decrements/increments are passed to function calls.
+    Call evaluateExpression(a + b).
+    This returns evaluateExpression(a) + evaluateExpression(b).
+    evaluateExpression(a) attempts to resolve a by searching backwards through the BIL code from lineNo.
+    It increments back until it finds a non-statement, or an assignment statement with lhs == a.
+    Once it finds this assignment statement, it returns evaluateExpression on the rhs of the statement.
+    If we hit a line that isn't a statement, we fail the evaluation and return null.
+
+    fixme: This method is incomplete: it only handles addition and subtraction of literals, and only evaluates move
+    assignments. A warning is given if a non-move assignment is discovered while backtracking through the code, as this
+    assignment may change the value of a variable we are trying to evaluate, resulting in an incorrect evaluation when
+    skipped.
     */
-    // incomplete: only handles addition and subtraction of literals, and only evaluates move assignments
-    // lineNo is the line this expression is on
     private Integer evaluateExpression(BilParser.ExpContext ectx, int lineNo) {
         if (ectx.getClass().equals(BilParser.ExpLiteralContext.class)) {
             return Integer.decode(ectx.getText());
@@ -194,7 +241,8 @@ public class BoogieTranslator {
                 if (assignment.exp().getClass().equals(BilParser.ExpLoadContext.class) ||
                         assignment.exp().getClass().equals(BilParser.ExpStoreContext.class)) {
                     // we don't evaluate non-move assignments yet. todo
-                    System.err.println("Can't guarantee accuracy of variable substitution as a non-move assignment was encountered");
+                    System.err.printf("Can't guarantee accuracy of variable substitution for %s as a non-move " +
+                            "assignment was encountered%n", var);
                     continue;
                 }
                 if (!assignment.var().getText().equals(var)) {
@@ -207,6 +255,10 @@ public class BoogieTranslator {
         return null;
     }
 
+    /**
+     * Handles a jump statement.
+     * @param ctx the jump to handle.
+     */
     private void handleJump(BilParser.JmpContext ctx) {
         String target = "";
         if (ctx.var() != null) {
@@ -217,12 +269,20 @@ public class BoogieTranslator {
         writeToFile(String.format("goto label%s", target));
     }
 
+    /**
+     * Handles a conditional-jump statement.
+     * @param ctx the conditional-jump to handle.
+     */
     private void handleCJump(BilParser.CjmpContext ctx) {
         String cond = ctx.var().getText();
         String target = ctx.addr().getText();
         writeToFile(String.format("if (%s) {goto label%s}", cond, target));
     }
 
+    /**
+     * Handles a call statement.
+     * @param ctx the call to handle.
+     */
     private void handleCall(BilParser.CallContext ctx) {
         // fixme: bill parser is having trouble understanding when we reach the end of a function call, and sometimes throws a null pointer exception if it doesn't think the end was reached
         try {
@@ -232,6 +292,10 @@ public class BoogieTranslator {
         }
     }
 
+    /**
+     * Handles a sub (i.e. function/procedure) line.
+     * @param ctx the sub to handle.
+     */
     private void handleSub(BilParser.SubContext ctx) {
         String functionName = ctx.functionName().getText();
         List<BilParser.ParamContext> params = ctx.param();
@@ -242,11 +306,21 @@ public class BoogieTranslator {
         funcName = functionName;
     }
 
+    /**
+     * Handles an end-sub line.
+     * E.g. "call LR with noreturn".
+     * @param ctx the end-sub to handle.
+     */
     private void handleEndSub(BilParser.EndsubContext ctx) {
         writeToFile(String.format("}%n"));
         funcName = "";
     }
 
+    /**
+     * Handles a paramtypes line.
+     * These are lines which appear after sub lines, that denote the parameters of that sub.
+     * @param ctx the paramtypes to handle.
+     */
     private void handleParam(BilParser.ParamTypesContext ctx) {
         String id = ctx.param().getText();
         if (id.contains("result")) {
@@ -271,62 +345,71 @@ public class BoogieTranslator {
         }
     }
 
-    private String parseExpression(BilParser.ExpContext ectx) {
+    /**
+     * Translates the given expression into boogie.
+     * Currently works for binary operations, unary operations, variables, literals and extraction expressions.
+     * @param ctx the expression to translate.
+     * @return the boogie translation of the expression.
+     */
+    private String parseExpression(BilParser.ExpContext ctx) {
         // expressions can be binary operations, unary operations, variables, literals or extractions (i.e. bit slices)
-        ectx = ignoreUnhandledContexts(ectx);
-
-        if (ectx.getClass().equals(BilParser.ExpBopContext.class)) {
-            /* Binary operation expression */
-            BilParser.ExpBopContext ctx = (BilParser.ExpBopContext) ectx;
-            String left = parseExpression(ctx.exp(0));
-            String right = parseExpression(ctx.exp(1));
-            String op = ctx.bop().getText();
-            if (op.equals("=")) {
-                op = "=="; // a little patching here, a little there...
-            }
-            return String.format("(%s) %s (%s)", left, op, right);
-
-        } else if (ectx.getClass().equals(BilParser.ExpUopContext.class)) {
-            /* Unary operation expression */
-            BilParser.ExpUopContext ctx = (BilParser.ExpUopContext) ectx;
-            String exp = parseExpression(ctx.exp());
-            String op = ctx.uop().getText();
-            return String.format("%s (%s)", op, exp);
-        } else if (ectx.getClass().equals(BilParser.ExpVarContext.class)) {
-            /* Variable expression */
-            BilParser.ExpVarContext ctx = (BilParser.ExpVarContext) ectx;
-            // vars can be flags, registers (X[num]) or "expression bundles" (#[num]) where [num] is some integer
-            String variable = ctx.var().getText();
-            if (variable.charAt(0) == 'X') {
-                // variable is a register, replace with call to register map
-                return parseRegister(variable);
-            } else if (variable.charAt(0) == '#') {
-                // variable is an expression bundle todo
-                return variable;
-            } else {
-                return variable;
-            }
-
-        } else if (ectx.getClass().equals(BilParser.ExpLiteralContext.class)) {
-            /* Literal expression */
-            BilParser.ExpLiteralContext ctx = (BilParser.ExpLiteralContext) ectx;
-            return ctx.literal().getText();
-
-        } else if (ectx.getClass().equals(BilParser.ExpExtractContext.class)) {
-            /* Extraction expression */
-            BilParser.ExpExtractContext ctx = (BilParser.ExpExtractContext) ectx;
-            int firstNat = Integer.parseInt(ctx.nat(0).getText());
-            int secondNat = Integer.parseInt(ctx.nat(1).getText());
-            String exp = parseExpression(ctx.exp());
-            // fixme: big warning! this is broken! assumes all bit vectors are 64 bits long
-            // wasn't sure how to get the length of a bit vector: might have to get it from somewhere else in the program and keep track of a variable
-            return String.format("%s[%d:%d]", exp, 64-firstNat, 64-secondNat-1); // fixme: in future, we want to properly translate exp before jamming it in here
-
-        } else {
-            System.err.println("Unhandled expression detected: " + ectx.getText());
-            return "";
+        ctx = ignoreUnhandledContexts(ctx);
+        if (ctx.getClass().equals(BilParser.ExpBopContext.class)) {
+            return parseBinaryOperation((BilParser.ExpBopContext) ctx);
+        } else if (ctx.getClass().equals(BilParser.ExpUopContext.class)) {
+            return parseUnaryOperation((BilParser.ExpUopContext) ctx);
+        } else if (ctx.getClass().equals(BilParser.ExpVarContext.class)) {
+            return parseVariableExpression((BilParser.ExpVarContext) ctx);
+        } else if (ctx.getClass().equals(BilParser.ExpLiteralContext.class)) {
+            return parseLiteralExpression((BilParser.ExpLiteralContext) ctx);
+        } else if (ctx.getClass().equals(BilParser.ExpExtractContext.class)) {
+            return parseExtractionExpression((BilParser.ExpExtractContext) ctx);
         }
+        System.err.println("Unhandled expression detected: " + ctx.getText());
+        return "";
+    }
 
+    private String parseBinaryOperation(BilParser.ExpBopContext ctx) {
+        String left = parseExpression(ctx.exp(0));
+        String right = parseExpression(ctx.exp(1));
+        String op = ctx.bop().getText();
+        if (op.equals("=")) {
+            op = "=="; // a little patching here, a little there...
+        }
+        return String.format("(%s) %s (%s)", left, op, right);
+    }
+
+    private String parseUnaryOperation(BilParser.ExpUopContext ctx) {
+        String exp = parseExpression(ctx.exp());
+        String op = ctx.uop().getText();
+        return String.format("%s (%s)", op, exp);
+    }
+
+    private String parseVariableExpression(BilParser.ExpVarContext ctx) {
+        // vars can be flags, registers (X[num]) or "expression bundles" (#[num]) where [num] is some integer
+        String variable = ctx.var().getText();
+        if (variable.charAt(0) == 'X') {
+            // variable is a register, replace with call to register map
+            return parseRegister(variable);
+        } else if (variable.charAt(0) == '#') {
+            // variable is an expression bundle todo
+            return variable;
+        } else {
+            return variable;
+        }
+    }
+
+    private String parseLiteralExpression(BilParser.ExpLiteralContext ctx) {
+        return ctx.literal().getText();
+    }
+
+    private String parseExtractionExpression(BilParser.ExpExtractContext ctx) {
+        int firstNat = Integer.parseInt(ctx.nat(0).getText());
+        int secondNat = Integer.parseInt(ctx.nat(1).getText());
+        String exp = parseExpression(ctx.exp());
+        // fixme: big warning! this is broken! assumes all bit vectors are 64 bits long
+        // wasn't sure how to get the length of a bit vector: might have to get it from somewhere else in the program and keep track of a variable
+        return String.format("%s[%d:%d]", exp, 64-firstNat, 64-secondNat-1); // fixme: in future, we want to properly translate exp before jamming it in here
     }
 
     /**
