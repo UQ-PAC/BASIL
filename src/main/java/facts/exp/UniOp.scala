4a82e5f10bcd49d03cf3ae22d6e736effe131dff
James Tobler <jamesjtobler@gmail.com>
2021-12-02 11:44:32 +1000

    created second constructors for uniOp and binOp, to convert string bil operators into Operator objects

--- src/main/java/facts/exp/UniOp.scala
+++ src/main/java/facts/exp/UniOp.scala
@@ -6,21 +6,22 @@ import java.util.Objects
 
 /** Unary operator fact
   */
-class UniOp(var operator: String, var exp: Expr) extends Expr {
+class UniOp(var operator: UniOperator.Value, var exp: Expr) extends Expr {
+  def this(operatorStr: String, exp: Expr) = this(UniOperator.fromBil(operatorStr), exp)
   override def toString = String.format("%s %s", operator, exp)
   override def getChildren = Collections.singletonList(exp)
   override def replace(oldExp: Expr, newExp: Expr) = if (exp == oldExp) exp = newExp
 
   override def vars: List[Var] = exp.vars
+}
 
-  object Operator extends Enumeration {
-    type Operator = Value
-    val UnaryNegation: Operator = Value("!")
-    val BitwiseComplement: Operator = Value("~") // todo
+object UniOperator extends Enumeration {
+  type Operator = Value
+  val UnaryNegation: Operator = Value("!")
+  val BitwiseComplement: Operator = Value("~") // todo
 
-    def fromBil(bilStr: String): Value = bilStr match {
-      case "-" => UnaryNegation
-      case "~" => BitwiseComplement
-    }
+  def fromBil(bilStr: String): Value = bilStr match {
+    case "-" => UnaryNegation
+    case "~" => BitwiseComplement
   }
 }
