9a44ce95d17028b764e48d98bf0f3f4b7cb12877
tomdx <thomasle0@outlook.com>
2022-04-27 10:52:06 +1000

    Add more grammar rules for ADT Implement more AST node generations in AdtListener

--- src/main/antlr4/BilAdt.g4
+++ src/main/antlr4/BilAdt.g4
@@ -1,55 +1,157 @@
 grammar BilAdt;
 
+adt : exp
+    | endian
+    | unimplemented
+    | list
+    | tuple;
 
-item : (SYMBOL OPEN_PAREN sequence CLOSE_PAREN) | OPEN_BRACKET sequence CLOSE_BRACKET | OPEN_PAREN sequence CLOSE_PAREN| STRING | NUM | REGISTER;
+exp : load
+    | store
+    | binop
+    | uop
+    | var
+    | intAdt
+    | cast
+    | extract
+    | STRING
+    | NUM
+    | REGISTER;
 
-sequence : (item (COMMA item)*)?;
+endian : ENDIAN OPEN_PAREN CLOSE_PAREN;
 
-NEWLINE : '\r'? '\n' -> skip;
-WHITESPACE : ' '+ -> skip;
-COMMENT : '//' ~[\r\n]* -> skip;
+// Load(mem, idx, endian, size)
+load : 'Load' OPEN_PAREN var COMMA exp COMMA endian COMMA NUM CLOSE_PAREN;
+store : 'Store' OPEN_PAREN var COMMA exp COMMA endian COMMA NUM CLOSE_PAREN;
 
+// BINOP(exp1, exp2) -- e.g. PLUS(exp1, exp2)
+binop : BINOP OPEN_PAREN exp COMMA exp CLOSE_PAREN;
+
+// UOP(exp) -- e.g. NOT(exp1)
+uop : UOP OPEN_PAREN COMMA exp CLOSE_PAREN;
+
+// var(name, type)
+var : 'Var' OPEN_PAREN STRING COMMA type CLOSE_PAREN;
+
+// Int(num, size)
+intAdt : 'Int' OPEN_PAREN NUM COMMA NUM CLOSE_PAREN;
+
+// CAST() -- e.g. UNSIGNED(size, expr)
+cast : CAST OPEN_PAREN NUM COMMA exp CLOSE_PAREN;
+
+// Let(var, val, expr) -- Unimplemented
+
+// Unknown(string, type) -- Unimplemented
+
+// Ite(cond, if_true, if_false) -- Unimplemented
+
+// Extract(hb, lb, exp)
+extract : 'Extract' OPEN_PAREN NUM COMMA NUM COMMA exp CLOSE_PAREN;
+
+// Concat(lhs, rhs) -- Unimplemented
+
+type : imm | mem;
+
+imm : 'Imm' OPEN_PAREN NUM CLOSE_PAREN;
+
+mem : 'Mem' OPEN_PAREN NUM NUM CLOSE_PAREN;
+
+// 'Tid'(number, name)
+tid : 'Tid' OPEN_PAREN NUM COMMA STRING CLOSE_PAREN;
+
+// 'Def'(
+def : 'Def' OPEN_PAREN tid COMMA adt COMMA adt COMMA adt CLOSE_PAREN; // this is an assignment, they're called defs in the ADT.
+call : 'Call' OPEN_PAREN tid COMMA adt COMMA adt COMMA adt CLOSE_PAREN;
+
+list : OPEN_BRACKET sequence CLOSE_BRACKET;
+
+tuple : OPEN_PAREN sequence CLOSE_PAREN;
+
+/* Unimportant ADTs - should be matched last */
+unimplemented : (SYMBOL OPEN_PAREN sequence CLOSE_PAREN);
+
+sequence : (adt (COMMA adt)*)?;
+
+BINOP : PLUS
+      | MINUS
+      | TIMES
+      | DIVIDE
+      | SDIVIDE
+      | MOD
+      | SMOD
+      | LSHIFT
+      | RSHIFT
+      | ARSHIFT
+      | AND
+      | OR
+      | XOR
+      | EQ
+      | NEQ
+      | LT
+      | LE
+      | SLT
+      | SLE;
+
+UOP : NEG | NOT;
+
+CAST : UNSIGNED | SIGNED | HIGH | LOW;
+
+UNSIGNED : 'Unsigned';
+SIGNED : 'Signed';
+HIGH : 'High';
+LOW : 'Low';
+
+// BinOp alternatives
+PLUS     : 'PLUS';
+MINUS    : 'MINUS';
+TIMES    : 'TIMES';
+DIVIDE   : 'DIVIDE';
+SDIVIDE  : 'SDIVIDE';
+MOD      : 'MOD';
+SMOD     : 'SMOD';
+LSHIFT   : 'LSHIFT';
+RSHIFT   : 'RSHIFT';
+ARSHIFT  : 'ARSHIFT';
+AND      : 'AND';
+OR       : 'OR';
+XOR      : 'XOR';
+EQ       : 'EQ';
+NEQ      : 'NEQ';
+LT       : 'LT';
+LE       : 'LE';
+SLT      : 'SLT';
+SLE      : 'SLE';
+
+// UnOp alternatives
+NOT      : 'NOT';
+NEG      : 'NEG';
+
+ENDIAN : LITTLE_ENDIAN | BIG_ENDIAN;
+LITTLE_ENDIAN : 'LittleEndian';
+BIG_ENDIAN : 'BigEndian';
+
+// Numbers and symbols
 SYMBOL : ALPHA+;
 ALPHA : [A-Za-z];
-
 NUM : DEC | HEX;
 DEC : DIGIT+;
 HEX : '0x'? HEXDIGIT+;
 REGISTER : 'R' DEC;
-
 DIGIT : [0-9];
 HEXDIGIT : [0-9a-fA-F];
 
+// Strings
 ESCAPE : '\\' ( '"' | '\\' | 'n' | 'x');
 STRING :  '"' ( ESCAPE | ~('"' | '\\' | '\n' | '\r') )+ '"' ;
 
-
+// Delimiters
 OPEN_PAREN : '(';
 CLOSE_PAREN : ')';
 COMMA : ',';
-
-
 OPEN_BRACKET : '[';
 CLOSE_BRACKET : ']';
 
-
-//program : KW_PROGRAM OPEN_PAREN tid COMMA attrs COMMA subs CLOSE_PAREN;
-//
-//keyword : KW_PROJECT | KW_PROGRAM | KW_UNDEFINED;
-//
-//attrs: KW_ATTRS unused;
-//
-////arg : 'Tid' '(' NUM '"%' NUM '"' ')';
-//subs : KW_SUBS unused;
-////subs : OPEN_BRACKET (sub (COMMA sub)*)? CLOSE_BRACKET;
-//
-//tid : KW_TID unused;
-//sub : KW_SUB unused; //KW_SUB OPEN_BRACKET  CLOSE_BRACKET;
-
-//unused : OPEN_PAREN (~CLOSE_PAREN | unused)* CLOSE_PAREN;
-
-//ADDR : ('%'|'#') HEXDIGIT+;
-//UNUSED_ID : ALPHA (ALPHA | DIGIT)*;
-//QUOTE : '"' ;
-//LT : '<';
-//GT : '>';
+// Ignored
+NEWLINE : '\r'? '\n' -> skip;
+WHITESPACE : ' '+ -> skip;
+COMMENT : '//' ~[\r\n]* -> skip;
--- src/main/scala/translating/AdtListener.scala
+++ src/main/scala/translating/AdtListener.scala
@@ -1,20 +1,24 @@
 package translating
 
 import BilParser.{BilAdtListener, BilAdtParser}
-import astnodes.exp.Expr
-import astnodes.exp.`var`.Register
+import astnodes.exp.{BinOp, Expr, Extend, Extract, Literal, Pad, UniOp}
+import astnodes.exp.`var`.{MemLoad, Register, Var}
 import astnodes.pred.Pred
 import astnodes.sec.{Sec, SecVar}
 import astnodes.stmt.{EnterSub, Stmt}
+import org.antlr.v4.runtime
 import org.antlr.v4.runtime.ParserRuleContext
 import org.antlr.v4.runtime.tree.{ErrorNode, ParseTree, ParseTreeProperty, TerminalNode}
+import util.AssumptionViolationException
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 class AdtListener extends BilAdtListener {
+
   val stmts = ArrayBuffer[Stmt]()
   private val exprs = ParseTreeProperty[Expr]
+
   private def getExpr(node: ParseTree) =
     if (node == null) throw new NullPointerException("Null expression")
     if (exprs.get(node) != null)
@@ -23,6 +27,7 @@ class AdtListener extends BilAdtListener {
       throw new Exception("Unparsed expression " + node.getText + " (" + node.getClass + ")")
 
   private val preds = ParseTreeProperty[Pred]
+
   private def getPred(node: ParseTree) =
     if (node == null) throw new NullPointerException("Null pred")
     if (preds.get(node) != null) preds.get(node)
@@ -30,6 +35,7 @@ class AdtListener extends BilAdtListener {
 
 
   private val secs = ParseTreeProperty[Sec]
+
   private def getSec(node: ParseTree) =
     if (node == null) throw new NullPointerException("Null security")
     if (secs.get(node) != null) secs.get(node)
@@ -49,67 +55,124 @@ class AdtListener extends BilAdtListener {
   var requires: List[Pred] = List()
   var ensures: List[Pred] = List()
 
-  private def getSubItem(ctx: BilAdtParser.ItemContext, name: String): BilAdtParser.ItemContext = {
-    for ( i <- 0 to ctx.sequence().item().size() - 1
-          if ctx.sequence().item(i).SYMBOL().getText == name) {
-        return ctx.sequence.item(i);
-      }
-    throw new Exception("Could not find SubItem: " + name)
+  override def enterAdt(ctx: BilAdtParser.AdtContext): Unit = ???
+
+  override def exitAdt(ctx: BilAdtParser.AdtContext): Unit = ???
+
+  override def enterExp(ctx: BilAdtParser.ExpContext): Unit = ???
+
+  override def exitExp(ctx: BilAdtParser.ExpContext): Unit = ???
+
+  override def enterEndian(ctx: BilAdtParser.EndianContext): Unit = ???
+
+  override def exitEndian(ctx: BilAdtParser.EndianContext): Unit = ???
+
+  override def enterLoad(ctx: BilAdtParser.LoadContext): Unit = {
+    if (ctx.`var`.STRING().getText == "mem") exprs.put(ctx, new MemLoad(getExpr(ctx.exp), Some(ctx.NUM().getText.toInt)))
+    else throw new AssumptionViolationException("Found load on variable other than mem")
   }
 
-  //private def getAttr(AttrsCtx: BilAdtParser.ItemContext): BilAdtParser.ItemContext = {
-  //
-  //}
+  override def exitLoad(ctx: BilAdtParser.LoadContext): Unit = ???
 
-  /**
-   * Enter a parse tree produced by {@link BilAdtParser#   item}.
-   *
-   * @param ctx the parse tree
-   */
-  override def enterItem(ctx: BilAdtParser.ItemContext): Unit = {
-    //ctx.SYMBOL().getText() match {
-    //}
-  }
+  override def enterStore(ctx: BilAdtParser.StoreContext): Unit = ???
 
-  /**
-   * Exit a parse tree produced by {@link BilAdtParser# item}.
-   *
-   * @param ctx the parse tree
-   */
-  override def exitItem(ctx: BilAdtParser.ItemContext): Unit = {
-    ctx.SYMBOL().getText() match {
-      case "Sub" =>
-        if (currentFunction != null)
-          currentFunction.setRequiresEnsures(requires, ensures)
+  override def exitStore(ctx: BilAdtParser.StoreContext): Unit = ???
 
-        requires = List()
-        ensures = List()
+  override def enterBinop(ctx: BilAdtParser.BinopContext): Unit = exprs.put(ctx, new BinOp(ctx.BINOP.getText, getExpr(ctx.exp(0)), getExpr(ctx.exp(1))))
 
-        val name = getSubItem(ctx, "Tid").sequence().item(1);
-        //val name = ctx.functionName.getText
+  override def exitBinop(ctx: BilAdtParser.BinopContext): Unit = ???
 
-        val function = new EnterSub(null, name, List(), List())
-        stmts += function
-        this.currentFunction = function
-      case "Def" =>
-        
+  override def enterUop(ctx: BilAdtParser.UopContext): Unit = exprs.put(ctx, new UniOp(ctx.UOP.getText, getExpr(ctx.exp)))
 
+  override def exitUop(ctx: BilAdtParser.UopContext): Unit = ???
+
+  override def enterVar(ctx: BilAdtParser.VarContext): Unit = {
+    if (ctx.STRING.getText != "mem") {  // Is a register
+      if (ctx.`type`.imm == null)
+        throw new AssumptionViolationException("Can't find Imm argument for a non-mem variable")
+      exprs.put(ctx, new Register(ctx.getText, Some(ctx.`type`.imm.NUM.getText.toInt)))
     }
   }
 
+  override def exitVar(ctx: BilAdtParser.VarContext): Unit = ???
 
-  /**
-   * Enter a parse tree produced by {@link BilAdtParser# sequence}.
-   *
-   * @param ctx the parse tree
-   */
+  override def enterIntAdt(ctx: BilAdtParser.IntAdtContext): Unit = {
+    exprs.put(ctx, Literal(ctx.NUM(0).getText)) // We also have access to size information if needed. But current AST node doesn't ask for it
+  }
+
+  override def exitIntAdt(ctx: BilAdtParser.IntAdtContext): Unit = ???
+
+  // Currently doesn't handle high/low. These could be converted to extracts
+  override def enterCast(ctx: BilAdtParser.CastContext): Unit = {
+    ctx.CAST.getText match {
+      case "pad" => exprs.put(ctx, Pad(getExpr(ctx.exp), ctx.NUM.getText.toInt))
+      case "extend" => exprs.put(ctx, Extend(getExpr(ctx.exp), ctx.NUM.getText.toInt))
+      case "low" | "high" => exprs.put(ctx, getExpr(ctx.exp))
+    }
+  }
+
+  override def exitCast(ctx: BilAdtParser.CastContext): Unit = ???
+
+  override def enterExtract(ctx: BilAdtParser.ExtractContext): Unit = {
+    val first = ctx.NUM(0).getText.toInt;
+    val second = ctx.NUM(1).getText.toInt;
+    val exp = getExpr(ctx.exp)
+    exprs.put(ctx, new Extract(first, second, exp));
+  }
+
+  override def exitExtract(ctx: BilAdtParser.ExtractContext): Unit = ???
+
+  override def enterType(ctx: BilAdtParser.TypeContext): Unit = ???
+
+  override def exitType(ctx: BilAdtParser.TypeContext): Unit = ???
+
+  override def enterImm(ctx: BilAdtParser.ImmContext): Unit = ???
+
+  override def exitImm(ctx: BilAdtParser.ImmContext): Unit = ???
+
+  override def enterMem(ctx: BilAdtParser.MemContext): Unit = ???
+
+  override def exitMem(ctx: BilAdtParser.MemContext): Unit = ???
+
+  override def enterTid(ctx: BilAdtParser.TidContext): Unit = ???
+
+  override def exitTid(ctx: BilAdtParser.TidContext): Unit = ???
+
+  override def enterDef(ctx: BilAdtParser.DefContext): Unit = {
+    if (currentFunction != null)
+      currentFunction.setRequiresEnsures(requires, ensures)
+
+    requires = List()
+    ensures = List()
+
+    val tid = ctx.tid.NUM.getText
+    val name = ctx.tid.STRING.getText
+
+    val function = new EnterSub(tid, name, List(), List())
+    stmts += function
+    this.currentFunction = function
+  }
+
+  override def exitDef(ctx: BilAdtParser.DefContext): Unit = ???
+
+  override def enterCall(ctx: BilAdtParser.CallContext): Unit = ???
+
+  override def exitCall(ctx: BilAdtParser.CallContext): Unit = ???
+
+  override def enterList(ctx: BilAdtParser.ListContext): Unit = ???
+
+  override def exitList(ctx: BilAdtParser.ListContext): Unit = ???
+
+  override def enterTuple(ctx: BilAdtParser.TupleContext): Unit = ???
+
+  override def exitTuple(ctx: BilAdtParser.TupleContext): Unit = ???
+
+  override def enterUnimplemented(ctx: BilAdtParser.UnimplementedContext): Unit = ???
+
+  override def exitUnimplemented(ctx: BilAdtParser.UnimplementedContext): Unit = ???
+
   override def enterSequence(ctx: BilAdtParser.SequenceContext): Unit = ???
 
-  /**
-   * Exit a parse tree produced by {@link BilAdtParser# sequence}.
-   *
-   * @param ctx the parse tree
-   */
   override def exitSequence(ctx: BilAdtParser.SequenceContext): Unit = ???
 
   override def visitTerminal(node: TerminalNode): Unit = ???
