6975e2c8425b2292cdae2c8ac3c878bfb290bb30
tomdx <thomasle0@outlook.com>
2022-06-02 14:30:31 +1000

    Run scalafmt on AdtListener

--- src/main/scala/translating/AdtListener.scala
+++ src/main/scala/translating/AdtListener.scala
@@ -2,7 +2,19 @@ package translating
 
 import BilParser.BilAdtParser.{DirectContext, ExpContext, ExpIntAdtContext, ExpVarContext, IndirectContext}
 import BilParser.{BilAdtBaseListener, BilAdtListener, BilAdtParser}
-import astnodes.exp.{BinOp, BinOperator, Expr, Extend, Extract, FunctionCall, Literal, MemStore, Pad, UniOp, UniOperator}
+import astnodes.exp.{
+  BinOp,
+  BinOperator,
+  Expr,
+  Extend,
+  Extract,
+  FunctionCall,
+  Literal,
+  MemStore,
+  Pad,
+  UniOp,
+  UniOperator
+}
 import astnodes.exp.`var`.{MemLoad, Register, Var}
 import astnodes.parameters.OutParameter
 import astnodes.parameters.InParameter
@@ -37,7 +49,6 @@ class AdtListener extends BilAdtBaseListener {
     if (preds.get(node) != null) preds.get(node)
     else throw new Exception("Unparsed pred " + node.getText + " (" + node.getClass + ")")
 
-
   private val secs = ParseTreeProperty[Sec]
 
   private def getSec(node: ParseTree) =
@@ -47,7 +58,6 @@ class AdtListener extends BilAdtBaseListener {
 
   val lPreds = new mutable.HashMap[Register, Sec]
   val gammaMappings = new mutable.HashMap[Register, SecVar]
-
 
   val varSizes = mutable.Map[String, Int]()
 
@@ -59,14 +69,15 @@ class AdtListener extends BilAdtBaseListener {
   var requires: List[Pred] = List()
   var ensures: List[Pred] = List()
 
-  /**
-   * For some reason, numbers in the ADT are stored as (Num, Type) where Num is the unsigned representation of the number,
-   * and type is the type (e.g. a type of 64 means 64-bit two's complement). It's assumed that it is always two's complement.
-   *
-   * For instance - Int(18446744073709551584,64) means -32.
-   *
-   * TODO: This takes and returns strings to conform with the current state. It should be converted to take and return BigInts long term
-   */
+  /** For some reason, numbers in the ADT are stored as (Num, Type) where Num is the unsigned representation of the
+    * number, and type is the type (e.g. a type of 64 means 64-bit two's complement). It's assumed that it is always
+    * two's complement.
+    *
+    * For instance - Int(18446744073709551584,64) means -32.
+    *
+    * TODO: This takes and returns strings to conform with the current state. It should be converted to take and return
+    * BigInts long term
+    */
   def castTo2sComplement(num: String, t: String): String = {
     val n = BigInt(num);
     val exponent = t.toInt;
@@ -78,10 +89,8 @@ class AdtListener extends BilAdtBaseListener {
     }
   }
 
-  /**
-   * Remove the quotation marks from a string
-   * e.g. "\"mem\"" becomes "mem"
-   */
+  /** Remove the quotation marks from a string e.g. "\"mem\"" becomes "mem"
+    */
   def getStringBody(str: String): String = {
     var newStr: String = str;
     if (newStr.charAt(0) == '"') {
@@ -95,22 +104,24 @@ class AdtListener extends BilAdtBaseListener {
 
   override def exitExpLoad(ctx: BilAdtParser.ExpLoadContext): Unit = {
     ctx.memexp match {
-      case v: ExpVarContext => if (getStringBody(v.STRING.getText) == "mem") {
-        exprs.put(ctx, new MemLoad(getExpr(ctx.exp(1)), Some(ctx.NUM().getText.toInt)))
-      } else {
-        throw new AssumptionViolationException("Found load on on variable other than mem")
-      }
+      case v: ExpVarContext =>
+        if (getStringBody(v.STRING.getText) == "mem") {
+          exprs.put(ctx, new MemLoad(getExpr(ctx.exp(1)), Some(ctx.NUM().getText.toInt)))
+        } else {
+          throw new AssumptionViolationException("Found load on on variable other than mem")
+        }
       case v: ExpContext => throw new AssumptionViolationException("Expected ExpVarContext, but got " + v.getClass())
     }
   }
 
   override def exitExpStore(ctx: BilAdtParser.ExpStoreContext): Unit = {
     ctx.memexp match {
-      case v: ExpVarContext => if (getStringBody(v.STRING.getText) == "mem") {
-        exprs.put(ctx, new MemStore(getExpr(ctx.idx), getExpr(ctx.value), Some(ctx.NUM().getText.toInt)))
-      } else {
-        throw new AssumptionViolationException("Found store on on variable other than mem")
-      }
+      case v: ExpVarContext =>
+        if (getStringBody(v.STRING.getText) == "mem") {
+          exprs.put(ctx, new MemStore(getExpr(ctx.idx), getExpr(ctx.value), Some(ctx.NUM().getText.toInt)))
+        } else {
+          throw new AssumptionViolationException("Found store on on variable other than mem")
+        }
       case v: ExpContext => throw new AssumptionViolationException("Expected ExpVarContext, but got " + v.getClass())
     }
   }
@@ -118,10 +129,11 @@ class AdtListener extends BilAdtBaseListener {
   override def exitExpBinop(ctx: BilAdtParser.ExpBinopContext): Unit =
     exprs.put(ctx, new BinOp(BinOperator.fromAdt(ctx.BINOP.getText), getExpr(ctx.exp(0)), getExpr(ctx.exp(1))))
 
-  override def exitExpUop(ctx: BilAdtParser.ExpUopContext): Unit = exprs.put(ctx, new UniOp(UniOperator.fromAdt(ctx.UOP.getText), getExpr(ctx.exp)))
+  override def exitExpUop(ctx: BilAdtParser.ExpUopContext): Unit =
+    exprs.put(ctx, new UniOp(UniOperator.fromAdt(ctx.UOP.getText), getExpr(ctx.exp)))
 
   override def exitExpVar(ctx: BilAdtParser.ExpVarContext): Unit = {
-    if (getStringBody(ctx.STRING.getText) != "mem") {  // Is a register
+    if (getStringBody(ctx.STRING.getText) != "mem") { // Is a register
       if (ctx.`type`.imm == null)
         throw new AssumptionViolationException("Can't find Imm argument for a non-mem variable")
       exprs.put(ctx, new Register(getStringBody(ctx.name.getText), Some(ctx.`type`.imm.NUM.getText.toInt)))
@@ -133,7 +145,10 @@ class AdtListener extends BilAdtBaseListener {
   }
 
   override def exitExpIntAdt(ctx: BilAdtParser.ExpIntAdtContext): Unit = {
-    exprs.put(ctx, Literal(ctx.NUM(0).getText)) // We also have access to size information if needed. But current AST node doesn't ask for it
+    exprs.put(
+      ctx,
+      Literal(ctx.NUM(0).getText)
+    ) // We also have access to size information if needed. But current AST node doesn't ask for it
   }
 
   // Currently doesn't handle high/low. These could be converted to extracts
@@ -141,8 +156,8 @@ class AdtListener extends BilAdtBaseListener {
   // TODO: the thing being casted
   override def exitExpCast(ctx: BilAdtParser.ExpCastContext): Unit = {
     ctx.CAST.getText match {
-      case "UNSIGNED" => exprs.put(ctx, Pad(getExpr(ctx.exp), ctx.NUM.getText.toInt))
-      case "SIGNED" => exprs.put(ctx, Extend(getExpr(ctx.exp), ctx.NUM.getText.toInt))
+      case "UNSIGNED"     => exprs.put(ctx, Pad(getExpr(ctx.exp), ctx.NUM.getText.toInt))
+      case "SIGNED"       => exprs.put(ctx, Extend(getExpr(ctx.exp), ctx.NUM.getText.toInt))
       case "LOW" | "HIGH" => exprs.put(ctx, getExpr(ctx.exp))
     }
   }
@@ -153,7 +168,6 @@ class AdtListener extends BilAdtBaseListener {
     val exp = getExpr(ctx.exp)
     exprs.put(ctx, new Extract(first, second, exp));
   }
-
 
   override def exitDef(ctx: BilAdtParser.DefContext): Unit = {
     val address = getStringBody(ctx.tid.name.getText);
@@ -184,7 +198,7 @@ class AdtListener extends BilAdtBaseListener {
     ctx.cond match {
       case v: ExpVarContext => {
         val cond = Register(getStringBody(v.name.getText), v.asInstanceOf[ExpVarContext].`type`.imm.NUM.getText.toInt);
-        if (ctx.target.indirect != null ) {
+        if (ctx.target.indirect != null) {
           val variable: ExpVarContext = ctx.target.indirect.exp.asInstanceOf[ExpVarContext]
           stmts += new CJmpStmt(address, getStringBody(variable.name.getText), "TODO", cond);
         } else if (ctx.target.direct != null) {
@@ -194,8 +208,8 @@ class AdtListener extends BilAdtBaseListener {
       case v: ExpIntAdtContext => {
         // Assume that if it's an int, the int evaluates to true. This seems to be the case
         // 99% of the time. It probably doesn't make sense to a cjump based on a falsy literal.
-        if (ctx.target.indirect != null ) {
-            val variable: ExpVarContext = ctx.target.indirect.exp.asInstanceOf[ExpVarContext]
+        if (ctx.target.indirect != null) {
+          val variable: ExpVarContext = ctx.target.indirect.exp.asInstanceOf[ExpVarContext]
           stmts += new JmpStmt(address, getStringBody(variable.name.getText))
         } else if (ctx.target.direct != null) {
           stmts += new JmpStmt(address, getStringBody(ctx.target.direct.tid.name.getText))
@@ -234,12 +248,12 @@ class AdtListener extends BilAdtBaseListener {
     val function = new EnterSub(address, name, List(), List())
     stmts += function
     this.currentFunction = function
-    for ( i <- 0 to ctx.args().arg.size() - 1) {
+    for (i <- 0 to ctx.args().arg.size() - 1) {
       var arg = ctx.args.arg(i)
 
       val id = getStringBody(arg.tid.name.getText)
       arg.rhs match {
-        case v:ExpVarContext => {
+        case v: ExpVarContext => {
           val name = v.name.getText
           val size = v.`type`.imm.NUM.getText.toInt
           if (arg.intent.getText.contains("in")) {
