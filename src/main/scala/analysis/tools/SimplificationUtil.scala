50cb100f89fe0e4c134a8d2a61b2810839258d53
l-kent <56100168+l-kent@users.noreply.github.com>
2022-06-07 11:20:46 +1000

    clean up simplificationutil a little

--- src/main/scala/analysis/tools/SimplificationUtil.scala
+++ src/main/scala/analysis/tools/SimplificationUtil.scala
@@ -13,19 +13,20 @@ case object SimplificationUtil {
   /**
     * Simplifies a concat expressions. Assumes every concat is either a pad or extend.
     */
-  def bitvecConcat(concat: Concat): Expr = {
-    if (concat.left.isInstanceOf[Literal] && concat.left.asInstanceOf[Literal].value.equals("0")) simplifyPad(concat)
-    else if (concat.right.isInstanceOf[Literal] && concat.right.asInstanceOf[Literal].value.equals("0")) simplifyExtend(concat)
-    else concat
+  def bitvecConcat(concat: Concat): Expr = concat.left match {
+    case l: Literal if l.value == "0" => simplifyPad(concat)
+    case _ => concat.right match {
+      case r: Literal if r.value == "0" => simplifyExtend(concat)
+      case _ => concat
+    }
   }
-
 
   /**
     * Simplifies a concat that appends 0s to lhs of bitvector.
     */
   private def simplifyPad(concat: Concat): Expr = {
     concat.right match {
-      // Due to the nature of BIL extdn & pad expressions, it is assumed the value returned from them will always be a bitvector of size 64
+      // Due to the nature of BIL extend & pad expressions, it is assumed the value returned from them will always be a bitvector of size 64
       case literal: Literal => Literal(literal.value, Some(literal.size.getOrElse(32) + concat.left.size.getOrElse(32)))
       case _ => concat
     }
@@ -37,7 +38,7 @@ case object SimplificationUtil {
   private def simplifyExtend(concat: Concat): Expr = {
     concat.left match {
       case literal: Literal =>
-        // Due to the nature of BIL extdn & pad expressions, it is assumed the value returned from them will always be a bitvector of size 64
+        // Due to the nature of BIL extend & pad expressions, it is assumed the value returned from them will always be a bitvector of size 64
         val lhsExtended = Integer.parseInt(literal.value) << concat.right.size.getOrElse(0)
         Literal(lhsExtended.toString, Some(literal.size.getOrElse(32) + concat.right.size.getOrElse(32)))
       case _ => concat
@@ -77,27 +78,22 @@ case object SimplificationUtil {
     * Simplifies arithmetic expressions.
     */
   def binArithmetic(binOp: BinOp): Expr = {
-    var newLhs: Expr = binOp.firstExp
-    var newRhs: Expr = binOp.secondExp
-    if (binOp.firstExp.isInstanceOf[BinOp]) {
-      newLhs = binArithmetic(binOp.firstExp.asInstanceOf[BinOp])
+    val newLhs = binOp.firstExp match {
+      case b: BinOp => binArithmetic(b)
+      case _ => binOp.firstExp
     }
-    if (binOp.secondExp.isInstanceOf[BinOp]) {
-      newRhs = binArithmetic(binOp.secondExp.asInstanceOf[BinOp])
-    }
-
-    if (newLhs.isInstanceOf[Literal] && newRhs.isInstanceOf[Literal]) {
-      val solution = performArithmetic(BigInt(binOp.firstExp.asInstanceOf[Literal].value), BigInt(binOp.secondExp.asInstanceOf[Literal].value), binOp.operator.toString)
-      return Literal(solution.toString)
+    val newRhs = binOp.secondExp match {
+      case b: BinOp => binArithmetic(b)
+      case _ => binOp.secondExp
     }
 
-    if (newLhs.isInstanceOf[Literal] && BigInt(newLhs.asInstanceOf[Literal].toString) == BigInt(0) && binOp.operator.equals("|")) {
-      return newRhs
-    } else if (newRhs.isInstanceOf[Literal] && BigInt(newRhs.asInstanceOf[Literal].toString) == BigInt(0) && binOp.operator.equals("|")) {
-      return newLhs
+    (newLhs, newRhs) match {
+      case (l: Literal, r: Literal) =>
+        Literal(performArithmetic(BigInt(l.value), BigInt(r.value), binOp.operator.toString).toString)
+      case (l: Literal, r: _) if BigInt(l.toString) == BigInt(0) && binOp.operator.equals("|") => newRhs
+      case (l: _, r: Literal) if BigInt(r.toString) == BigInt(0) && binOp.operator.equals("|") => newLhs
+      case _ => binOp.copy(firstExp = newLhs, secondExp = newRhs)
     }
-
-    binOp.copy(firstExp = newLhs, secondExp = newRhs)
   }
 
   /**
