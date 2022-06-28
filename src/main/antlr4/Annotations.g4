7d2840a72f86243c2f9ffcea963a98cb8259654a
tomdx <thomasle0@outlook.com>
2022-06-28 15:53:19 +1000

    Implement most of remaining methods in AnnotationsLoader.scala Need a way to handle references to registers (in the case of Lpreds or gammas)

--- src/main/antlr4/Annotations.g4
+++ src/main/antlr4/Annotations.g4
@@ -1,20 +1,31 @@
 grammar Annotations;
 
+// This is the grammar for the security specification - it is mostly a copy-paste from the
+// end of the Bil.g4 file
 progSpec : (lpreds | gammas | lattice | rely)*;
 
 lpreds : 'L:' lpred (COMMA lpred)*;
-lpred : id MAPSTO expr;
+lpred : var=id MAPSTO secExpr;
 gammas : 'GAMMA:' gamma (COMMA gamma)*;
-gamma : id MAPSTO id;
-lattice: 'Lattice' lattice_elem (COMMA lattice_elem)* ;
+gamma : var=id MAPSTO id;
+lattice: 'Lattice:' lattice_elem (COMMA lattice_elem)* ;
 
 // FIXME: Not sure if the 'ID' string is meant to be an ID token, but this was the implementation in the old annotations
 //  parser (which was integrated as part of the old bil parser).
 lattice_elem : ID '<:' 'ID'*;
 
-rely: 'Rely:' expr;
+rely: 'Rely:' pred;
 
-expr : e0;
+secExpr :
+	'if' pred 'then' secExpr 'else' secExpr #secITE
+	| ID 		  # secLatticeElem
+	;
+
+/* PREDICATES */
+
+// This section is for predicates which are represented as a boogie expression - grammar found here:
+// https://www.microsoft.com/en-us/research/wp-content/uploads/2016/12/krml178.pdf
+pred : e0;
 e0 : exp=e1 | lhs=e1 EQUIV_OP rhs=e0;
 e1 : exp=e2 | lhs=e2 IMPL_OP rhs=e1;
 e2 : exp=e3 | exp=e3 eOr+ | exp=e3 eAnd+;
@@ -25,21 +36,21 @@ e4 : exp=e5 | lhs=e4 CONCAT_OP rhs=e5;
 e5 : exp=e6 | lhs=e5 ADD_OP rhs=e6;
 e6 : exp=e7 | lhs=e6 MUL_OP rhs=e7;
 e7 : UN_OP* exp=e8;
-e8 : e9 mapOp*;
+e8 : exp=e9 mapOp*;
 
 e9 : KW_FALSE
    | KW_TRUE
    | NUMBER
    // BITVEC
-   | ID funcApplication
-   | KW_OLD LPAREN expr RPAREN
-   | LPAREN QOP typeArgs? idsType (COMMA idsType)* QSEP trigAttr* expr RPAREN
-   | LPAREN expr RPAREN
+   | ID funcApplication?
+   | KW_OLD LPAREN pred RPAREN
+   | LPAREN QOP typeArgs? idsType (COMMA idsType)* QSEP trigAttr* pred RPAREN
+   | LPAREN pred RPAREN
    ;
 
-mapOp : LBRACKET expr (COMMA expr)* mapUpdate? RBRACKET | LBRACKET NUMBER COLON NUMBER RBRACKET;
+mapOp : LBRACKET pred (COMMA pred)* mapUpdate? RBRACKET | LBRACKET NUMBER COLON NUMBER RBRACKET;
 
-mapUpdate : '::=' expr;
+mapUpdate : '::=' pred;
 idsType : id (COMMA id)* COLON type;
 
 type : typeAtom | mapType | id typeCtorArgs?;
@@ -47,18 +58,18 @@ typeAtom : KW_BOOL | KW_INT | KW_BV NUMBER | LPAREN type RPAREN;
 mapType : typeArgs? LBRACKET type (COMMA type)* RBRACKET type;
 typeCtorArgs : typeAtom typeCtorArgs? | id typeCtorArgs? | mapType;
 typeArgs : LANGLE id (COMMA id)* RANGLE;
-funcApplication : LPAREN (expr (COMMA expr)*)? RPAREN;
+funcApplication : LPAREN (pred (COMMA pred)*)? RPAREN;
 
 trigAttr : trigger | attribute;
 
 // Bitvector
 bitVector : NUMBER KW_BV NUMBER;
 
-trigger : LCURLY expr+ RCURLY;
+trigger : LCURLY pred+ RCURLY;
 attribute : LCURLY COLON ID attrArg* RCURLY;
-attrArg : expr | STRING;
+attrArg : pred | STRING;
 
-id : IDENTIFIER;
+id : ID;
 
 COMMA : ',';
 COLON : ':';
@@ -97,6 +108,7 @@ NUMBER : [0-9]+;
 MAPSTO : '->';
 // Technically this can support all non-english unicode letters as well
 // and explicitly supports the following symbols [-, ., $, @, ', `, ~, ^, \, ?].
+ID : [a-zA-Z-.$@'`~^\\?][a-zA-Z-.$@'`~^\\?0-9]*;
 
 // Strings
 ESCAPE : '\\' ( '"' | '\\' | 'n' | 'x' | '.');
--- src/main/scala/translating/AnnotationsLoader.scala
+++ src/main/scala/translating/AnnotationsLoader.scala
@@ -2,12 +2,21 @@ package translating
 
 import BilParser.{AnnotationsBaseListener, AnnotationsListener}
 import BilParser.AnnotationsParser
-import astnodes.pred.{BinOp, ExprComp, Pred, UniOp}
+import BilParser.AnnotationsParser.{GammaContext, LpredContext, RelyContext}
+import astnodes.exp.`var`.Register
+import astnodes.pred.{BinOp, ExprComp, Pred, UniOp, Var}
+import astnodes.sec.{Sec, SecVar}
 import org.antlr.v4.runtime.tree.{ParseTree, ParseTreeProperty}
+
+import scala.collection.mutable
 
 class AnnotationsLoader extends AnnotationsBaseListener {
 
   private val preds = ParseTreeProperty[Pred]
+  val lPreds = new mutable.HashMap[Register, Sec]
+  val gammaMappings = new mutable.HashMap[Register, SecVar]
+  var rely: Option[Pred] = None
+  private val secs = ParseTreeProperty[Sec]
 
   def getPred(node: ParseTree): Pred = {
     if (node == null) throw new NullPointerException("Null pred")
@@ -15,6 +24,10 @@ class AnnotationsLoader extends AnnotationsBaseListener {
     else throw new Exception("Unparsed pred " + node.getText + " (" + node.getClass + ")")
 
   }
+  private def getSec(node: ParseTree) =
+    if (node == null) throw new NullPointerException("Null security")
+    if (secs.get(node) != null) secs.get(node)
+    else throw new Exception("Unparsed security " + node.getText + " (" + node.getClass + ")")
 
   override def exitE0(ctx: AnnotationsParser.E0Context): Unit = {
     if ctx.EQUIV_OP != null then preds.put(ctx, new BinOp(ctx.EQUIV_OP.getText, getPred(ctx.lhs), getPred(ctx.rhs)))
@@ -62,11 +75,49 @@ class AnnotationsLoader extends AnnotationsBaseListener {
     }
     preds.put(ctx, pred);
   }
-  override def exitE8(ctx: AnnotationsParser.E8Context): Unit = {}
-  override def exitE9(ctx: AnnotationsParser.E9Context): Unit = {
-    preds.put(ctx, )
+  override def exitE8(ctx: AnnotationsParser.E8Context): Unit = {
+    // TODO: Implement e8 - MAP operations
+    preds.put(ctx, getPred(ctx.exp));
   }
-  override def exitEOr(ctx: AnnotationsParser.EOrContext): Unit = {}
-  override def exitEAnd(ctx: AnnotationsParser.EAndContext): Unit = {}
+  override def exitE9(ctx: AnnotationsParser.E9Context): Unit = {
+    // TODO: Finish implementing e9
+    // TODO: Implement literals
+    if (ctx.KW_FALSE != null) {
+    } else if (ctx.KW_TRUE != null) {
+    } else if (ctx.NUMBER != null) {
+    } else if (ctx.ID != null) {
+      if (ctx.funcApplication != null) {
+        // TODO: Implement function call
+      } else {
+        preds.put(ctx, new Var(ctx.ID.getText))
+      }
+    } else if (ctx.KW_OLD != null) {
+      // TODO: Implement Old nodes (if needd)
+    } else if (ctx.QOP != null) {
+      // TODO: Implement quantifier nodes
+    } else {
+      preds.put(ctx, getPred(ctx.pred));
+    }
+  }
+  
+  override def exitSecLatticeElem(ctx: BilParser.AnnotationsParser.SecLatticeElemContext): Unit = secs.put(ctx, SecVar(ctx.getText))
+
+
+  // TODO: Not sure how to handle references to registers? i.e. ctx.var. It seems like the specification being in a
+  //  separate file makes it impossible to resolve this.
+  override def exitGamma(ctx: GammaContext): Unit = {
+    //getExpr(ctx.`var`) match {
+    //  case v: Register => gammaMappings.put(v, SecVar(ctx.id(1).getText))
+    //}
+  }
+  // TODO: Not sure how to handle references to registers? i.e. ctx.var. It seems like the specification being in a
+  //  separate file makes it impossible to resolve this.
+  override def exitLpred(ctx: LpredContext): Unit = {
+    //(getExpr(ctx.`var`), getSec(ctx.secExpr)) match {
+    //  case (v: Register, p: Sec) => lPreds.put(v, p)
+    //}
+  }
+
+  override def exitRely(ctx: RelyContext): Unit = rely = Some(getPred(ctx.pred))
 
 }
