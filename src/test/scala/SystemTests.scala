4b330dbe02c02f08474e9760135468bee88f7ef2
James Tobler <64625414+j-tobler@users.noreply.github.com>
2022-10-10 23:37:14 +1000

    generalised system tests so that benchmark programs are automatically detected

--- src/test/scala/SystemTests.scala
+++ src/test/scala/SystemTests.scala
@@ -1,4 +1,8 @@
 import org.scalatest.funsuite.AnyFunSuite
+import java.io.File
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+
 
 /*
 In future, we could have a predefined structure for test file names and locations for a cleaner and
@@ -7,12 +11,28 @@ more manageable test suite.
 
 
 class SystemTests extends AnyFunSuite {
-  val globalTestDirectory = "examples/"
+  val programsDir = "./src/test/programs"
+  val programs = getTests(programsDir)
 
-  test("secret_write") {
-    val directory = globalTestDirectory + "secret_write/"
-    main(directory + "secret_write.adt",
-      directory + "secret_write.relf",
-      directory + "secret_write.spec")
+  programs.foreach(t => test(t) {
+    val stdPath = "%s/%s/%s".format(programsDir, t, t)
+    val actualOutPath = stdPath + "_actual_out.bpl"
+    val expectedOutPath = stdPath + "_expected_out.bpl"
+    main(stdPath + ".adt", stdPath + ".relf", stdPath + ".spec", actualOutPath)
+    // check success/failure of verification matches expectation
+      // todo: run boogie?
+    // check that boogie output matches expected
+    if(!Source.fromFile(actualOutPath).getLines.mkString.equals(
+      Source.fromFile(expectedOutPath).getLines().mkString)) {
+      fail("boogie file differs from expected")
+    }
+  })
+
+  // gets the names all subdirectories in src/test/programs
+  def getTests(directoryName: String): Array[String] = {
+    (new File(directoryName))
+      .listFiles
+      .filter(_.isDirectory)
+      .map(_.getName)
   }
 }
