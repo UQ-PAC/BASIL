0e714313674da140850223f9f78f11fc028c2dd1
yousifpatti <yousifpatti@gmail.com>
2023-04-12 15:08:36 +1000

    Fixed tests

--- src/test/scala/MemoryRegionAnalysisMiscTest.scala
+++ src/test/scala/MemoryRegionAnalysisMiscTest.scala
@@ -9,14 +9,25 @@ class MemoryRegionAnalysisMiscTest extends AnyFunSuite with OneInstancePerTest {
   //C:\workdir\bil-to-boogie-translator\examples
   private val examplesPath = System.getProperty("user.dir") + "/examples/";
   private val expectedPath = System.getProperty("user.dir") + "/src/test/scala/dotExpected/";
+  private val tempPath = System.getProperty("user.dir") + "/src/test/scala/dump/";
   def runMain(name: String, dump: Boolean = false): Unit = {
     var expected = ""
-    var output = ""
+    var actual = ""
+    var output: Option[Map[analysis.CfgNode, ?]] = null
     RunUtils.generateVCsAdt(examplesPath + s"${name}/${name}.adt", examplesPath + s"${name}/${name}.relf", None, true)
     try {
-      output = RunUtils.memoryRegionAnalysisResults.toString
+      output = RunUtils.memoryRegionAnalysisResults
+      val outFile = new File(tempPath + s"${name}")
+      val pw = new PrintWriter(outFile, "UTF-8")
+      output.get.foreach {
+        case (k, v) =>
+          pw.write(s"${k} -> ${v}")
+          pw.write("\n")
+      }
+      pw.close()
+      val actualFile = scala.io.Source.fromFile(tempPath + s"${name}")
+      actual = actualFile.mkString
       val expectedFile = scala.io.Source.fromFile(expectedPath + s"${name}")
-      print(output)
       expected = expectedFile.mkString
       expectedFile.close()
     } catch {
@@ -24,14 +35,18 @@ class MemoryRegionAnalysisMiscTest extends AnyFunSuite with OneInstancePerTest {
         if (dump) {
           val outFile = new File(expectedPath + s"${name}")
           val pw = new PrintWriter(outFile, "UTF-8")
-          pw.write(output)
+          output.get.foreach {
+            case (k, v) =>
+              pw.write(s"${k} -> ${v}")
+              pw.write("\n")
+          }
           pw.close()
           assert(true)
           return
         }
         throw new Exception("TEST NOT SUPPORTED: Expected file not found " + expectedPath + s"${name}")
     }
-    assert(output == expected)
+    assert(actual.split('\n').sorted.mkString("Array(", ", ", ")") == expected.split('\n').sorted.mkString("Array(", ", ", ")"))
   }
 
 //  def compareDots(name: String): Unit = {
@@ -70,8 +85,8 @@ class MemoryRegionAnalysisMiscTest extends AnyFunSuite with OneInstancePerTest {
     runMain("basicfree");
   }
 
-//  // used to generate the expected files (DO NOT RUN)
+  // used to generate the expected files (DO NOT RUN)
 //  test("generate") {
-//    runMain("basicfree", true);
+//    runMain("ifglobal", true);
 //  }
 }
