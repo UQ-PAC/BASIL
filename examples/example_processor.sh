3b7cffc5d3a7cc07df108daa027e055ff00fa803
yousifpatti <yousifpatti@gmail.com>
2023-05-02 15:55:56 +1000

    Example processor a bit faster

--- examples/example_processor.sh
+++ examples/example_processor.sh
@@ -21,22 +21,26 @@ if ! command -v readelf &> /dev/null; then
     echo "Error: readelf is not installed"
     exit 1
 fi
+echo "Processing $1"
+while true; do
+  echo -n "."
+  sleep 0.5
+done &
 
 # get the filename without the extension
 filename=$(basename "$1")
 filename="${filename%.*}"
 
 # compile the C file into an executable
-aarch64-linux-gnu-gcc -fno-plt -fno-pic "$1" -o "${filename}.out"
+aarch64-linux-gnu-gcc -fno-plt -fPIC "$1" -o "${filename}.out"
 
 # generate the ADT file using bap
-bap --primus-lisp-semantics=disable "${filename}.out" -d adt:"${filename}.adt"
-
-# generate the BIR file using bap
-bap --primus-lisp-semantics=disable "${filename}.out" -d bir:"${filename}.bir"
+bap --primus-lisp-semantics=disable "${filename}.out" -d adt:"${filename}.adt" -d bir:"${filename}.bir"
 
 # generate the REFL file using readelf
 readelf -s -r -W "${filename}.out" > "${filename}.relf"
 
 # remove the executable
 rm "${filename}.out"
+kill $!
+echo " Done"
