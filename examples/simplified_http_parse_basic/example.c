9ce1c25e1d10bfc2ca6df3becd9beff86bb38f0e
Alistair Michael <alistair.michael@uq.edu.au>
2023-10-17 16:07:58 +1000

    update example to verifying

--- examples/simplified_http_parse_basic/example.c
+++ examples/simplified_http_parse_basic/example.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 
-#define MALLOC_SIZE 10
-
+#define MALLOC_SIZE 4
+// times out with 64 bit buffer
 
 // cntlm 22
 
 char *buf;
 char password = 7; // secret value; has to be a variable so that we get a Gamma_password variable
-char stext[11] = "sometext";
+char stext[11] = "hel";
 
 
 
@@ -17,18 +17,19 @@ int main() {
     char *pos = NULL, *dom = NULL;
     
 
-   stext[5] = password;
+   stext[2] = password;
    buf = malloc(strlen(stext) + 1);
    memcpy(buf, stext, strlen(stext) + 1); // inlined by -O2
 
    puts(buf);
 
    // find the split between username and password ("username:password")
-   pos = buf + 2;
+   pos = buf + 1;
+    
+  // including this makes verification fail
+ // *pos = 0;
 
-  *pos = 0;
-
-  memset(buf, 0, strlen(buf));
+  memset(buf, 0, strlen(buf) + 1);
   free(buf); // requires secret[i] == true
 
 }
