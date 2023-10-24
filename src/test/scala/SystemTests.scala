a1b4d3719bb9eb2adc5d1f6d4ddb066690c4364c
Alistair Michael <alistair.michael@uq.edu.au>
2023-10-24 15:21:40 +1000

    distinguish timeout from failure, add summary test

--- src/test/scala/SystemTests.scala
+++ src/test/scala/SystemTests.scala
@@ -11,15 +11,19 @@ import scala.sys.process.*
   * directory structure and file-name patterns.
   */
 class SystemTests extends AnyFunSuite {
+
   val testPath = "./src/test/"
   val correctPath = "./src/test/correct"
   val correctPrograms: Array[String] = getSubdirectories(correctPath)
   val incorrectPath = "./src/test/incorrect"
   val incorrectPrograms: Array[String] = getSubdirectories(incorrectPath)
 
-  case class TestResult(passed: Boolean, verified: Boolean, shouldVerify: Boolean, hasExpected: Boolean, matchesExpected: Boolean, translateTime: Long, verifyTime: Long) {
-    final val csvHeader = "passed,verified,shouldVerify,hasExpected,matchesExpected,translateTime,verifyTime"
-    val toCsv = s"$passed,$verified,$shouldVerify,$hasExpected,$matchesExpected,$translateTime,$verifyTime"
+  case class TestResult(passed: Boolean, verified: Boolean, shouldVerify: Boolean, hasExpected: Boolean, timedOut: Boolean, matchesExpected: Boolean, translateTime: Long, verifyTime: Long) {
+    val toCsv = s"$passed,$verified,$shouldVerify,$hasExpected,$timedOut,$matchesExpected,$translateTime,$verifyTime"
+  }
+
+  object TestResult {
+    val csvHeader = "passed,verified,shouldVerify,hasExpected,timedOut,matchesExpected,translateTime,verifyTime"
   }
 
   val testResults: mutable.Map[String, TestResult] = mutable.HashMap()
@@ -45,6 +49,27 @@ class SystemTests extends AnyFunSuite {
     )
   }
 
+  test("summary") {
+    val csv: String = "testCase," + TestResult.csvHeader + System.lineSeparator() + testResults.map(r => s"${r._1},${r._2.toCsv}").mkString(System.lineSeparator())
+    log(csv, testPath + "testResults.csv")
+
+    val numVerified = testResults.count(_._2.verified)
+    val numNotVerified = testResults.count(!_._2.verified)
+    val numTimeout = testResults.count(_._2.timedOut)
+    val verifying = testResults.filter(x => !x._2.timedOut && x._2.verified).map(_._2.verifyTime)
+    val counterExamples = testResults.filter(x => !x._2.timedOut && !x._2.verified).map(_._2.verifyTime)
+
+    info(s"Test summary: $numVerified verified, $numNotVerified failed to verify including $numTimeout timeouts.")
+    if (verifying.nonEmpty)
+      info(s"Average time to verify: ${verifying.sum / verifying.size}")
+    if (counterExamples.nonEmpty)
+      info(s"Average time to counterexample: ${counterExamples.sum/ counterExamples.size}")
+
+    val summaryHeader = "verifiedCount,counterexampleCount,timeoutCount,verifyTotalTime,counterexampleTotalTime"
+    val summaryRow = s"$numVerified,${counterExamples.size},$numTimeout,${verifying.sum},${counterExamples.sum}"
+    log(summaryHeader + System.lineSeparator() + summaryRow, testPath + "summary.csv")
+  }
+
 
   def runTest(path: String, name: String, variation: String, shouldVerify: Boolean): Unit= {
     val directoryPath = path + "/" + name + "/"
@@ -57,17 +82,18 @@ class SystemTests extends AnyFunSuite {
     Logger.info(outPath)
     val timer = PerformanceTimer(s"test $name/$variation")
     if (File(specPath).exists) {
-      Main.main(Array("--adt", ADTPath, "--relf", RELFPath, "--spec", specPath, "--output", outPath, "--dump-il", ilPath))
+      Main.main(Array("--adt", ADTPath, "--relf", RELFPath, "--spec", specPath, "--output", outPath, "--dump-il", ilPath, "--boogie-use-lambda-stores"))
     } else {
-      Main.main(Array("--adt", ADTPath, "--relf", RELFPath, "--output", outPath, "--dump-il", ilPath))
+      Main.main(Array("--adt", ADTPath, "--relf", RELFPath, "--output", outPath, "--dump-il", ilPath, "--boogie-use-lambda-stores"))
     }
     val translateTime = timer.checkPoint("translate-boogie")
     Logger.info(outPath + " done")
-    val boogieResult = Seq("boogie", "/timeLimit:10", "/printVerifiedProceduresCount:0", "/useArrayAxioms", outPath).!!
+    val boogieResult = Seq("boogie", "/timeLimit:10", "/printVerifiedProceduresCount:0", outPath).!!
     val verifyTime = timer.checkPoint("verify")
     val resultPath = variationPath + "_result.txt"
     log(boogieResult, resultPath)
     val verified = boogieResult.strip().equals("Boogie program verifier finished with 0 errors")
+    val timedOut = boogieResult.strip() contains "timed out"
     val failureMsg =
       if shouldVerify then "Expected verification success, but got failure."
       else "Expected verification failure, but got success."
@@ -84,10 +110,8 @@ class SystemTests extends AnyFunSuite {
       info("Note: this test has not previously succeeded")
     }
     val passed = verified == shouldVerify
-    val result = TestResult(passed, verified, shouldVerify, hasExpected, matchesExpected, translateTime, verifyTime)
+    val result = TestResult(passed, verified, shouldVerify, hasExpected, timedOut, matchesExpected, translateTime, verifyTime)
     testResults.put(s"$name/$variation", result)
-    val csv: String = "testCase," + result.csvHeader + System.lineSeparator() + testResults.map(r => s"${r._1},${r._2.toCsv}").mkString(System.lineSeparator())
-    log(csv, testPath + "summary.csv")
     if (!passed) fail(failureMsg)
   }
 
