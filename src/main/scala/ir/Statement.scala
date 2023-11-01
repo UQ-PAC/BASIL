2d75e770a5a1a2601954bd607f09d5cb00153e4d
l-kent <l.kent@uq.edu.au>
2023-11-01 12:27:52 +1000

    don't need to define apply for classes in scala 3 unless we want an alternate constructor

--- src/main/scala/ir/Statement.scala
+++ src/main/scala/ir/Statement.scala
@@ -22,10 +22,8 @@ class LocalAssign(var lhs: Variable, var rhs: Expr, override val label: Option[S
   override def acceptVisit(visitor: Visitor): Statement = visitor.visitLocalAssign(this)
 }
 
-
 object LocalAssign:
-  def apply(lhs: Variable, rhs: Expr, label: Option[String])  = new LocalAssign(lhs, rhs, label)
-  def unapply(l: LocalAssign) : Option[(Variable, Expr, Option[String])] = Some(l.lhs, l.rhs, l.label)
+  def unapply(l: LocalAssign): Option[(Variable, Expr, Option[String])] = Some(l.lhs, l.rhs, l.label)
 
 class MemoryAssign(var lhs: Memory, var rhs: MemoryStore, override val label: Option[String] = None) extends Statement {
   override def modifies: Set[Global] = Set(lhs)
@@ -35,8 +33,7 @@ class MemoryAssign(var lhs: Memory, var rhs: MemoryStore, override val label: Op
 }
 
 object MemoryAssign:
-  def apply(lhs: Memory, rhs: MemoryStore, label: Option[String]) = new MemoryAssign(lhs, rhs, label)
-  def unapply(m: MemoryAssign) : Option[(Memory, MemoryStore, Option[String])] = Some(m.lhs, m.rhs, m.label)
+  def unapply(m: MemoryAssign): Option[(Memory, MemoryStore, Option[String])] = Some(m.lhs, m.rhs, m.label)
 
 case object NOP extends Statement {
   override def label: Option[String] = None
@@ -50,9 +47,7 @@ class Assume(var body: Expr, var comment: Option[String], override val label: Op
 }
 
 object Assume:
-  def apply(body: Expr, comment: Option[String], label: Option[String] = None) = new Assume(body, comment, label)
-  def unapply(a: Assume) : Option[(Expr, Option[String], Option[String])] = Some(a.body, a.comment, a.label)
-
+  def unapply(a: Assume): Option[(Expr, Option[String], Option[String])] = Some(a.body, a.comment, a.label)
 
 class Assert(var body: Expr, var comment: Option[String], override val label: Option[String] = None) extends Statement {
   override def toString: String = s"assert $body" + comment.map(" //" + _)
@@ -60,8 +55,7 @@ class Assert(var body: Expr, var comment: Option[String], override val label: Op
 }
 
 object Assert:
-  def apply(body: Expr, comment: Option[String], label: Option[String] = None) : Assert = new Assert(body, comment, label)
-  def unapply(a: Assert) : Option[(Expr, Option[String], Option[String])] = Some(a.body, a.comment, a.label)
+  def unapply(a: Assert): Option[(Expr, Option[String], Option[String])] = Some(a.body, a.comment, a.label)
 
 trait Jump extends Command {
   def modifies: Set[Global] = Set()
@@ -80,9 +74,7 @@ class GoTo(var target: Block, var condition: Option[Expr], override val label: O
 }
 
 object GoTo:
-  def apply(target: Block, condition: Option[Expr], label: Option[String] = None) = new GoTo(target, condition, label)
-
-  def unapply(g: GoTo) : Option[(Block, Option[Expr])]= Some(g.target, g.condition)
+  def unapply(g: GoTo): Option[(Block, Option[Expr], Option[String])] = Some(g.target, g.condition, g.label)
 
 class DirectCall(var target: Procedure, var condition: Option[Expr], var returnTarget: Option[Block], override val label: Option[String] = None) extends Jump {
   /* override def locals: Set[Variable] = condition match {
@@ -94,13 +86,8 @@ class DirectCall(var target: Procedure, var condition: Option[Expr], var returnT
   override def acceptVisit(visitor: Visitor): Jump = visitor.visitDirectCall(this)
 }
 
-
 object DirectCall:
-  def apply(target: Procedure, condition: Option[Expr], returnTarget: Option[Block], label: Option[String] = None)
-  = new DirectCall(target, condition, returnTarget, label)
-
   def unapply(i: DirectCall): Option[(Procedure, Option[Expr], Option[Block], Option[String])] = Some(i.target, i.condition, i.returnTarget, i.label)
-
 
 class IndirectCall(var target: Variable, var condition: Option[Expr], var returnTarget: Option[Block],
                    override val label: Option[String] = None) extends Jump {
@@ -113,7 +100,4 @@ class IndirectCall(var target: Variable, var condition: Option[Expr], var return
 }
 
 object IndirectCall:
-  def apply(target: Variable, condition: Option[Expr], returnTarget: Option[Block], label: Option[String] = None)
-    = new IndirectCall(target, condition, returnTarget, label)
-
   def unapply(i: IndirectCall): Option[(Variable, Option[Expr], Option[Block], Option[String])] = Some(i.target, i.condition, i.returnTarget, i.label)
