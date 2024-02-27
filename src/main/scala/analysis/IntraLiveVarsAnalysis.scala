f8b77004378d2e006783d97801e38c8432eb8e41
l-kent <l.kent@uq.edu.au>
2024-02-27 10:01:05 +1000

    remember to handle other cfgpositions

--- src/main/scala/analysis/IntraLiveVarsAnalysis.scala
+++ src/main/scala/analysis/IntraLiveVarsAnalysis.scala
@@ -1,7 +1,7 @@
 package analysis
 
 import analysis.solvers.SimpleWorklistFixpointSolver
-import ir.{Assert, Assume, CFGPosition, Call, DirectCall, GoTo, IndirectCall, Jump, LocalAssign, MemoryAssign, NOP, Program, Statement, Variable}
+import ir.{Assert, Assume, Block, CFGPosition, Call, DirectCall, GoTo, IndirectCall, Jump, LocalAssign, MemoryAssign, NOP, Procedure, Program, Statement, Variable}
 
 abstract class LivenessAnalysis(program: Program) extends Analysis[Any]:
   val lattice: MapLattice[CFGPosition, Set[Variable], PowersetLattice[Variable]] = new MapLattice(new PowersetLattice())
@@ -9,6 +9,8 @@ abstract class LivenessAnalysis(program: Program) extends Analysis[Any]:
 
   def transfer(n: CFGPosition, s: Set[Variable]): Set[Variable] = {
     n match {
+      case p: Procedure => s
+      case b: Block => s
       case LocalAssign(variable, expr, _) => (s - variable) ++ expr.variables
       case MemoryAssign(_, store, _) => s ++ store.index.variables ++ store.value.variables
       case Assume(expr, _, _, _) => s ++ expr.variables
