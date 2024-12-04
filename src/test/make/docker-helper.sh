dc5f18cf107b2a7b02689c04f9ca912939059163
rina <k@rina.fyi>
2024-12-04 17:53:18 +1000

    implement docker hashing

--- src/test/make/docker-helper.sh
+++ src/test/make/docker-helper.sh
@@ -7,7 +7,14 @@ root=$(git rev-parse --show-toplevel)
 : ${DOCKER_IMAGE:=localhost/basil-tools-docker:latest}
 : ${DOCKER_CONTAINER:=basil-build-container}
 
-if [[ "$1" == start ]]; then
+FLAKE=github:katrinafyi/pac-nix/cceaf2eabacb584ef66c738f5e057f3fa9cbbd84#basil-tools-docker
+
+if [[ "$1" == build ]]; then
+  # safe to re-run. if docker image is already up-to-date, should be reasonably fast.
+  nix build "$FLAKE" --no-link
+  $(nix build "$FLAKE" --no-link --print-out-paths) | "$DOCKER" image load
+  exit
+elif [[ "$1" == start ]]; then
   set -x
 	exec $DOCKER run -v$GIT_ROOT:/build --rm -td --user root --name $DOCKER_CONTAINER $DOCKER_IMAGE
 elif [[ "$1" == stop ]]; then
--- src/test/make/lift.mk
+++ src/test/make/lift.mk
@@ -1,8 +1,6 @@
 
 # Run from the directory basil/src/test/*/test_case/compilation_variant/
 
-.DELETE_ON_ERROR:
-
 $(LIFT_ARTEFACTS): a.out
 	$(READELF) -s -r -W a.out > $(NAME).relf
 	$(BAP) a.out -d adt:$(NAME).adt -d bir:$(NAME).bir
@@ -10,13 +8,6 @@ $(LIFT_ARTEFACTS): a.out
 	proto-json.py --idem=proto -s8 $(NAME).temp.gtirb $(NAME).gtirb
 	rm $(NAME).temp.gtirb
 	gtirb-semantics $(NAME).gtirb $(NAME).gts
-ifeq ($(USE_DOCKER), 1)
-	$(DOCKER_CMD) hash > docker-hash-new
-	diff -u docker-hash-new docker-hash
-	md5sum -c md5sums  # using docker; checking compiler output hashes.
-else
-	md5sum -c md5sums  # not using docker; ignoring check errors.
-endif
 
 ifdef $(SPEC)
 BASIL_SPECARG = --spec $(SPEC) 
@@ -38,7 +29,7 @@ $(BASIL):
 a.out: $(C_SOURCE)
 	$(CC) $(CFLAGS) $(C_SOURCE)
 
-.PHONY=recompile verify md5sums-update clean cleanlift cleanall cleanbin cleantest cleangts json gts
+.PHONY=recompile verify md5sum-check md5sum-update clean cleanlift cleanall cleanbin cleantest cleangts json gts
 verify: $(NAME)_bap.bpl $(NAME)_gtirb.bpl
 
 recompile: a.out
@@ -57,7 +48,17 @@ $(NAME)_bap_result.txt: $(NAME)_bap.bpl $(EXTRA_SPEC)
 $(NAME)_gtirb_result.txt: $(NAME)_gtirb.bpl $(EXTRA_SPEC)
 	bash -c "time boogie $(NAME)_gtirb.bpl $(EXTRA_SPEC) $(BOOGIE_FLAGS) | tee $(NAME)_gtirb_result.txt"
 
-md5sums-update: a.out $(LIFT_ARTEFACTS)
+md5sum-check:
+ifeq ($(USE_DOCKER), 1)
+	$(DOCKER_CMD) hash > docker-hash-new
+	diff -u docker-hash-new docker-hash  # if this fails, make sure your docker image is up-to-date.
+	rm docker-hash-new
+	md5sum -c md5sums  # using docker; checking compiler output hashes.
+else
+	-md5sum -c md5sums  # not using docker; ignoring check errors.
+endif
+
+md5sum-update: a.out $(LIFT_ARTEFACTS)
 	md5sum *.* > md5sums
 	$(DOCKER_CMD) hash > docker-hash
 
