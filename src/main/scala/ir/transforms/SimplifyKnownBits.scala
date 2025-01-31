629cca65ccca29dd8e59e5d3b2f3ac87fd297203
morriisw <>
2025-01-31 23:13:20 +1000

    Simplify transfer method by moving expression matching functionality into a separate method called evaluateExprToTNum to allow for recursive matching and evaluation. Create new method called evaluateBinOp that evaluates binary operations and can be called within evaluateExprToTNum method.

--- src/main/scala/ir/transforms/SimplifyKnownBits.scala
+++ src/main/scala/ir/transforms/SimplifyKnownBits.scala
@@ -68,53 +68,107 @@ class TNumDomain extends AbstractDomain[Map[Variable, TNum]] {
             TNum(iv.value & mask, BigInt(0))
     }
 
+    // Evaluates binary operation
+    def evaluateBinOp(op: BVBinOp, tn1: TNum, tn2: TNum): TNum = {
+        op match {
+            case BVAND => tn1.TAND(tn2)
+            case BVOR => tn1.TOR(tn2)
+            case BVXOR => tn1.TXOR(tn2)
+            case _ => TNum(BigInt(0), BigInt(-1))
+        }
+    }
+
+    // Recursively evaluates nested or non-nested expression
+    def evaluateExprToTNum(s: Map[Variable, TNum], expr: Expr): TNum = expr match {
+        case b: BitVecLiteral => toTNum(b)
+
+        case i: IntLiteral => toTNum(i)
+
+        case v: Variable => s.getOrElse(v, TNum(BigInt(0), BigInt(-1)))
+
+        case BinaryExpr(op: BVBinOp, arg1: Expr, arg2: Expr) => 
+            val arg1TNum = evaluateExprToTNum(arg1)
+            val arg2TNum = evaluateExprToTNum(arg2)
+            evaluateBinOp(op, arg1TNum, arg2TNum)
+
+        case Extract(endIndex: Int, startIndex: Int, body: Expr) => 
+            val bodyTNum = evaluateExprToTNum(body)
+            val bodyTNumValueExtract = (bodyTNum.value >> startIndex) & ((BigInt(1) << (endIndex - startIndex)) - 1)
+            val bodyTNumMaskExtract = (bodyTNum.mask >> startIndex) & ((BigInt(1) << (endIndex - startIndex)) - 1)
+            TNum(bodyTNumValueExtract, bodyTNumMaskExtract)
+
+        case Repeat(repeats: Int, body: Expr) =>
+            val bodyTNum = evaluateExprToTNum(s, body)
+            val repeatedValue = (0 until repeats).foldLeft(BigInt(0)) { (acc, _) => (acc << bodyTNum.value.bitLength) | bodyTNum.value }
+            val repeatedMask = (0 until repeats).foldLeft(BigInt(0)) { (acc, _) => (acc << bodyTNum.mask.bitLength) | bodyTNum.mask }
+            TNum(repeatedValue, repeatedMask)
+
+        case ZeroExtend(extension: Int, body: Expr) =>
+            val bodyTNum = evaluateExprToTNum(s, body)
+            val newLength = bodyTNum.value.bitLength + extension
+            val zeroExtendedValue = bodyTNum.value & ((BigInt(1) << newLength) - 1)
+            val zeroExtendedMask = bodyTNum.mask & ((BigInt(1) << newLength) - 1)
+            TNum(zeroExtendedValue, zeroExtendedMask)
+
+        case SignExtend(extension: Int, body: Expr) =>
+            val bodyTNum = evaluateExprToTNum(s, body)
+            val valueMsb = (bodyTNum.value >> (bodyTNum.value.bitLength - 1)) & 1
+            val maskMsb = (bodyTNum.mask >> (bodyTNum.mask.bitLength - 1)) & 1
+
+            val (extendedValue, extendedMask) = (valueMsb, maskMsb) match {
+                case (0, 0) =>
+                    // If MSB of value is 0 and MSB of mask is 0, extend value and mask with 0
+                    (bodyTNum.value, bodyTNum.mask)
+                case (1, 0) =>
+                    // If MSB of value is 1 and MSB of mask is 0, extend value with 1 and mask with 0
+                    val extendedPart = (BigInt(1) << extension) - 1
+                    (bodyTNum.value | (extendedPart << bodyTNum.value.bitLength), bodyTNum.mask)
+                case (0, 1) =>
+                    // If MSB of value is 0 and MSB of mask is 1, extend value with 0 and mask with 1
+                    val extendedPartMask = (BigInt(1) << extension) - 1
+                    (bodyTNum.value, bodyTNum.mask | (extendedPartMask << bodyTNum.mask.bitLength))
+                case (1, 1) =>
+                    // If MSB 0 in value or mask has been removed due to BigInt type 
+                    // (e.g. value = 001 = BigInt(1) != MSB 0, mask = 100), compare their bit lengths 
+                    // since (value, mask) can never be (1, 1) and have the same length
+                    if (bodyTNum.value.bitLength > bodyTNum.mask.bitLength) {
+                        // If value has more bits, extend value with 1 and mask with 0
+                        val extendedPartValue = (BigInt(1) << extension) - 1
+                        (bodyTNum.value | (extendedPartValue << bodyTNum.value.bitLength), bodyTNum.mask)
+                    } else if (bodyTNum.value.bitLength < bodyTNum.mask.bitLength) {
+                        // If value has less bits, extend value with 0 and mask with 1
+                        val extendedPartMask = (BigInt(1) << extension) - 1
+                        (bodyTNum.value, bodyTNum.mask | (extendedPartMask << bodyTNum.mask.bitLength))
+                    }
+            }
+            TNum(extendedValue, extendedMask)
+
+        case UnaryExpr(op: BVNOT, arg: Expr) =>
+            val argTNum = evaluateExprToTNum(s, arg)
+            TNum(~argTNum.value & ~argTNum.mask, argTNum.mask)
+
+        case _ => TNum(BigInt(0), BigInt(-1))
+    }
+
     // s is the abstract state from previous command/block
     override def transfer(s: Map[Variable, TNum], b: Command): Map[Variable, TNum] = {
         b match {
-            // Binary expression assignment with two bitvectors (e.g. x = 0101 & 1010)
-            case LocalAssign(lhs: Variable, BinaryExpr(op: BinOp, arg1: BitVecLiteral, arg2: BitVecLiteral)) =>
-                val tn1 = toTNum(arg1)
-                val tn2 = toTNum(arg2)
-
-                op match {
-                    case BVAND => s.updated(lhs, tn1.TAND(tn2))
-                    case BVOR => s.updated(lhs, tn1.TOR(tn2))
-                    case BVXOR => s.updated(lhs, tn1.TXOR(tn2))
-                }
-
             // Assign variable to variable (e.g. x = y)
-            case LocalAssign(lhs: Variable, rhs: Variable) =>
-                if (s.contains(rhs)) { 
-                    s.updated(lhs, s(rhs)) 
-                } else {
-                    // Overapproxiate unknown variables with Top
-                    s.updated(lhs, TNum(BigInt(0), BigInt(-1)))
-                }
+            case LocalAssign(lhs: Variable, rhs: Expr) =>
+                s.updated(lhs, evaluateExprToTNum(rhs))
 
-            // Assign bitvector to variable (e.g. x = 0101)
-            case LocalAssign(lhs: Variable, rhs: BitVecLiteral) =>
-                s.updated(lhs, toTNum(rhs))
-
-            case LocalAssign(lhs: Variable, BinaryExpr(op: BinOp, arg1: IntLiteral, arg2: IntLiteral)) =>
-
-            case LocalAssign(lhs: Variable, BinaryExpr(op: BinOp, arg1: Variable, arg2: Variable)) =>
-            case LocalAssign(lhs: Variable, BinaryExpr(op: BinOp, arg1: Extract, arg2: Extract)) =>
-            case LocalAssign(lhs: Variable, BinaryExpr(op: BinOp, arg1: Repeat, arg2: Repeat)) =>
-
-            case MemoryLoad(lhs: Variable, mem: Memory, index: Expr, endian: Endian, size: Int) =>
-                if (s.contains(lhs)) {
-                    s
-                } else {
+            // Load from memory and store in variable
+            case MemoryLoad(lhs: Variable, mem: Memory, index: Expr, endian: Endian, size: Int) 
+                if !s.contains(lhs) =>
                     // Overapproxiate memory values with Top
                     s.updated(lhs, TNum(BigInt(0), BigInt(-1)))
-                }
 
             // Default case    
             case _ => s 
         }
     }
 
-    // Join the same variables and merge shared TNum values, otherwise replace with Top
+    // Joins the same variables and merges shared TNum values, otherwise replaces with Top
     // e.g. Join: x = 0011, x = 1111
     // x = 0011 => value = 0011, mask = 0000
     // x = 1111 => value = 1111, mask = 0000
