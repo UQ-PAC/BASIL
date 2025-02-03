31757de3318d116e82880a2715ef11ad8bc28cb5
bpaul <bpaul@bpaul.xyz>
2025-02-03 15:37:52 +1000

    Fix procedure summaries tests

--- src/main/scala/ir/IRCursor.scala
+++ src/main/scala/ir/IRCursor.scala
@@ -225,7 +225,7 @@ def stronglyConnectedComponents[T <: CFGPosition, O <: T](walker: IRWalk[T, O], 
       if (!vIndex.contains(next)) {
         strongconnect(next)
         vLowLink(cur) = vLowLink(cur).min(vLowLink(next))
-      } else {
+      } else if (vOnStack(next)) {
         vLowLink(cur) = vLowLink(cur).min(vIndex(next))
       }
     }
@@ -241,6 +241,8 @@ def stronglyConnectedComponents[T <: CFGPosition, O <: T](walker: IRWalk[T, O], 
       out += component
     }
   }
+  println(out)
+  assert(stack.size == 0)
 
   out.map(_.toSet).toList
 }
