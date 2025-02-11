dce665f617d3b14e707ceadb96de6db986e97a50
Sadra Bayat Tork <sbayattork@gmail.com>
2025-02-11 15:49:16 +1000

    improved assertions

--- src/main/scala/analysis/data_structure_analysis/SetDSA.scala
+++ src/main/scala/analysis/data_structure_analysis/SetDSA.scala
@@ -22,7 +22,7 @@ class SetGraph(proc: Procedure, phase: DSAPhase,
     val cell1 = find(c1)
     val cell2 = find(c2)
 
-    if cell1.equals(cell2) then
+    val result = if cell1.equals(cell2) then
       cell1
     else if cell1.node.equals(cell2.node) then
       cell1.node.merge(cell1, cell2)
@@ -30,6 +30,14 @@ class SetGraph(proc: Procedure, phase: DSAPhase,
       collapseAndMerge(cell1, cell2)
     else
       mergeCellsHelper(cell1, cell2)
+
+    assert(result == find(cell1))
+    assert(result == find(cell2))
+    if cell1.hasPointee then
+      assert(result.getPointee == cell1.getPointee)
+    if cell2.hasPointee then
+      assert(result.getPointee == cell2.getPointee)
+    result
 
   }
 
