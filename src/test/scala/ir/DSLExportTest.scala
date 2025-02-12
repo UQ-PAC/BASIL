dda7a80996d70fc15893aba40c71c9cbc4fe9567
rina <k@rina.fyi>
2025-02-12 17:54:01 +1000

    fix test

--- src/test/scala/ir/DSLExportTest.scala
+++ src/test/scala/ir/DSLExportTest.scala
@@ -9,7 +9,6 @@ import scala.runtime.stdLibPatches.Predef.assert
 
 class DSLExportTest extends AnyFunSuite {
 
-
   val program: Program = prog(
     proc("main",
       block("first_call",
@@ -38,12 +37,40 @@ class DSLExportTest extends AnyFunSuite {
     )
   )
 
-  test("asd") {
-    println((LocalAssign(R0, bv64(1), None)).toScala)
-    println((Assert(TrueLiteral, Some("asd"))).toScala)
-    println( program.toScala)
-    println((Assert(TrueLiteral, Some("asd"))).toScala)
-    assert(false)
+  val expected = """
+    prog(
+      proc("main",
+        block("first_call",
+          LocalAssign(Register("R0", 64), BitVecLiteral(BigInt("1"), 64), None),
+          LocalAssign(Register("R1", 64), BitVecLiteral(BigInt("1"), 64), None),
+          directCall("callee1""),
+          goto("second_call")
+        ),
+        block("second_call",
+          directCall("callee2""),
+          goto("returnBlock")
+        ),
+        block("returnBlock",
+          ret
+        )
+      ),
+      proc("callee1",
+        block("returnBlock",
+          ret
+        )
+      ),
+      proc("callee2",
+        block("returnBlock",
+          ret
+        )
+      )
+    )
+  """
+
+  def cleanOutput(s: String): String = s.replaceAll(" ", "").replaceAll("\n", "").trim
+
+  test("test basil ir to dsl") {
+    assert(cleanOutput(expected) == cleanOutput(program.toScala))
   }
 
 }
