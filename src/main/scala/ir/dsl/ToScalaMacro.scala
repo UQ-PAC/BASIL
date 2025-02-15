97d9418e1611b015c89b596fde5c39b3c93d36c9
rina <k@rina.fyi>
2025-02-15 19:40:24 +1000

    enum detection

--- src/main/scala/ir/dsl/ToScalaMacro.scala
+++ src/main/scala/ir/dsl/ToScalaMacro.scala
@@ -30,7 +30,7 @@ inline def deriveOrSummon[T, Elem]: Boop[Elem] =
 inline def deriveRec[T, Elem]: Boop[Elem] =
   inline erasedValue[T] match
     case _: Elem => error("infinite recursive derivation")
-    case _       => Boop.derived[Elem] // recursive derivation
+    case _       => Boop.derived[Elem](using summonInline[Mirror.Of[Elem]]) // recursive derivation
 
 inline def elemTypes[Elems <: Tuple](using quotes: Quotes): List[quotes.reflect.TypeRepr] =
   import quotes.reflect.*
@@ -44,36 +44,53 @@ def boopOfSum[T](s: Mirror.SumOf[T], elems: => List[Boop[?]]): Boop[T] =
       val idx = s.ordinal(x)
       elems(s.ordinal(x)).asInstanceOf[Boop[T]].boop(x)
 
-inline def boopOfProduct[T](s: Mirror.ProductOf[T], elems: => List[Boop[?]]): Boop[T] =
-  val tys = elemTypes[s.MirroredElemTypes]
+def boopOfProduct[T](p: Mirror.ProductOf[T], elems: => List[Boop[?]]): Boop[T] =
   new Boop[T]:
     def boop(x: T): String =
-
-      "boop of product"
-
+      // XXX: "not a constant type". just pull out all needed things within the derived function and pass through
+      constValue[p.MirroredLabel]
 
 trait Boop[A]:
   def boop(x: A): String
+
+
+case class Entity(id: Int, value: String) derives Boop
 
 // https://docs.scala-lang.org/scala3/reference/contextual/derivation-macro.html
 
 object Boop {
 
-  inline def derived[T]: Boop[T] = ${ derivedMacro[T] }
+  inline def derived[T](using m: Mirror.Of[T]): Boop[T] =
+    lazy val elemInstances = summonInstances[T, m.MirroredElemTypes] // (1)
+    println("hi")
 
-  inline def derivedMirror[T](m: Mirror.Of[T], instances: List[Boop[?]]): Boop[T] =
-    inline m match                                                   // (2)
-      case s: Mirror.SumOf[T]     => boopOfSum(s, elemInstances)
-      case p: Mirror.ProductOf[T] => boopOfProduct(p, elemInstances)
+    inline erasedValue[m.MirroredMonoType] match
+      case _: scala.reflect.Enum => println("is enum")
+      case _ => ()
 
+    val a = inline m match                                                   // (2)
+      case s: Mirror.SumOf[T]     =>
+        println("sum label " + constValue[s.MirroredLabel])
+        boopOfSum(s, elemInstances)
+      case p: Mirror.ProductOf[T] =>
+        println("prod label " + constValue[p.MirroredLabel])
+        // println(s"prod type ${p.MirroredType}")
 
-  def derivedMacro[T: Type](using Quotes): Expr[Boop[T]] =
-    import quotes.reflect.*
+        boopOfProduct[T](p, elemInstances)
 
-    val expr: Expr[Mirror.Of[T]] = Expr.summon[Mirror.Of[T]].get
-    expr match
-      case '{ $m: Mirror.Of[T] } => '{ derivedMirror[T]($m) }
-
-    //
+    println("bye")
+    a
 }
 
+enum EAAA {
+  case A
+  case B
+}
+
+given Boop[Int] = ???
+given Boop[String] = ???
+given Boop[None.type] = Boop.derived
+given Boop[ir.Endian] = Boop.derived
+given Boop[EAAA] = Boop.derived
+val x = summon[Boop[Entity]].boop(Entity(1, "a"))
+val y = summon[Boop[EAAA]].boop(EAAA.B)
