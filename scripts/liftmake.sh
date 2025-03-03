140663c5fc4724f5ba05d9645e162ef0ae5408b9
Alistair Michael <alistair.michael@uq.edu.au>
2025-03-03 10:39:56 +1000

    cleanup script

--- scripts/liftmake.sh
+++ scripts/liftmake.sh
@@ -10,9 +10,6 @@ case "${o}" in
     c)
         CFILE_NAME="$CFILE_NAME ${OPTARG}"
         ;;
-    j)
-        JOBS=${OPTARG}
-        ;;
     *) 
         if ${OPTARG} == "--" ; then
         	break;
@@ -23,15 +20,15 @@ shift $((OPTIND-1))
 
 if [[ -z "$CFILE_NAME" ]] ; then 
     echo "Usage: -c cfile.c [-b binaryname] [ -- make args]"
-    echo "Set variables CC, CFLAGS, "
+    echo "Optinoally variables CC, CFLAGS, DDISASM, READELF, GTIRBSEM, BAP to change the the binary path to the lifter tools"
     exit 1
 fi
 
-BIN_NAME=${BIN_NAME:=$(echo "$CFILE_NAME" | sed -s 's/.c$//')}
+BIN_NAME=${BIN_NAME:=$(echo "$CFILE_NAME" | sed -s 's/.c$//' | tr -d ' ')}
 export BIN_NAME
 export CFILE_NAME
 
-make $@ -f - << EOF
+make $@ -f - << 'EOF'
 CC ?= aarch64-linux-gnu-gcc
 DDISASM ?= ddisasm
 READELF ?= readelf
@@ -39,24 +36,24 @@ GTIRBSEM ?= gtirb_semantics
 BAP ?= bap
 
 .PHONY=all clean 
-all: \$(BIN_NAME).adt \$(BIN_NAME).relf \$(BIN_NAME).gts
+all: $(BIN_NAME).adt $(BIN_NAME).relf $(BIN_NAME).gts
 
-\$(BIN_NAME): \$(CFILE_NAME)
-	\$(CC) \$(CFILE_NAME) \$(CFLAGS)  -o \$(BIN_NAME)
+$(BIN_NAME): $(CFILE_NAME)
+	$(CC) $(CFILE_NAME) $(CFLAGS)  -o $(BIN_NAME)
 
-\$(BIN_NAME).adt: \$(BIN_NAME)
-	\$(BAP) \$(BIN_NAME) -d adt:\$(BIN_NAME).adt -d bir:\$(BIN_NAME).bir
+$(BIN_NAME).adt: $(BIN_NAME)
+	$(BAP) $(BIN_NAME) -d adt:$(BIN_NAME).adt -d bir:$(BIN_NAME).bir
 
-\$(BIN_NAME).relf: \$(BIN_NAME)
-	\$(READELF) -s -r -W \$(BIN_NAME) > \$(BIN_NAME).relf
+$(BIN_NAME).relf: $(BIN_NAME)
+	$(READELF) -s -r -W $(BIN_NAME) > $(BIN_NAME).relf
 
-\$(BIN_NAME).gts: \$(BIN_NAME).gtirb
-	\$(GTIRBSEM) \$(BIN_NAME).gtirb \$(BIN_NAME).gts
+$(BIN_NAME).gts: $(BIN_NAME).gtirb
+	$(GTIRBSEM) $(BIN_NAME).gtirb $(BIN_NAME).gts
 
-\$(BIN_NAME).gtirb: \$(BIN_NAME)
-	\$(DDISASM) \$(BIN_NAME) --ir \$(BIN_NAME).gtirb
+$(BIN_NAME).gtirb: $(BIN_NAME)
+	$(DDISASM) $(BIN_NAME) --ir $(BIN_NAME).gtirb
 
 clean:
-	rm -f \$(BIN_NAME).adt \$(BIN_NAME).relf \$(BIN_NAME).gts \$(BIN_NAME)
+	rm -f $(BIN_NAME).adt $(BIN_NAME).relf $(BIN_NAME).gts $(BIN_NAME)
 EOF
 
