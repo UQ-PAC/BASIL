dc3ae4e6eafdcec570335545d57c1c0a917aceae
rina <k@rina.fyi>
2025-03-07 16:29:36 +1000

    SATTest: remove smt.timeout option (it is set by command line)

--- src/test/scala/SATTest.scala
+++ src/test/scala/SATTest.scala
@@ -9,17 +9,9 @@ import translating.BasilIRToSMT2
 @test_util.tags.UnitTest
 class SATTest extends AnyFunSuite {
   test(" basic taut ") {
-
-    // FIXME: z3 reports an error: "unknown parameter 'timeout' at module 'smt'"
-    // caused by this line: (set-option :smt.timeout 1)
-    // does this need a specific z3 version??
-    pendingUntilFixed {
-
-      // Logger.setLevel(LogLevel.DEBUG)
-      val e = BinaryExpr(BoolEQ, BinaryExpr(BVNEQ, R0, bv64(0)), BinaryExpr(BVEQ, bv64(0), R0))
-      val r = BasilIRToSMT2.proveExpr(e)
-      assert(r == Some(true))
-
-    }
+    // Logger.setLevel(LogLevel.DEBUG)
+    val e = BinaryExpr(BoolEQ, BinaryExpr(BVNEQ, R0, bv64(0)), BinaryExpr(BVEQ, bv64(0), R0))
+    val r = BasilIRToSMT2.proveExpr(e)
+    assert(r == Some(true))
   }
 }
