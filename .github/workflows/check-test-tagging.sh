25a25caad7357e10d767e852336de642b6d00755
rina <k@rina.fyi>
2025-03-12 12:22:36 +1000

    add Slow tag, make check-test-tagging stricter

--- .github/workflows/check-test-tagging.sh
+++ .github/workflows/check-test-tagging.sh
@@ -10,7 +10,7 @@ echo "$tests"
 echo '::endgroup::'
 echo
 echo '::group::Disabled test suites:'
-grep '@test_util.tags.DisabledTest' --context=1 -R $test_dir
+grep --color=always '@test_util\.tags\.DisabledTest' --context=1 -R $test_dir
 echo '::endgroup::'
 echo
 
@@ -18,9 +18,11 @@ ok=true
 echo '::group::Test suites with no tag annotations:'
 for t in $tests; do
   defn="$(grep 'class\s\+'"$t"'[ (]' --before-context=2 -R $test_dir)"
-  if ! grep -q '@test_util.tags.' <<< "$defn"; then
-    echo 'test suite has no `@test_util.tags.*` annotation:' >&2
-    grep 'class\s\+'"$t"'[ (]' -R $test_dir
+  # search for lines which are entirely "@test_util.tags.*Test"
+  # leading - is produced by grep to mark prefixes
+  if ! grep -q -- '-@test_util\.tags\..\+Test$' <<< "$defn"; then
+    echo 'test suite has no `@test_util.tags.*Test` annotation:' >&2
+    grep --color=always 'class\s\+'"$t"'[ (]' --before-context=2  -R $test_dir
     echo
     ok=false
   fi
