a45653662fbed57f0e89a4f4070469b8e1ac0faf
Sadra Bayat Tork <sbayattork@gmail.com>
2025-03-18 14:38:31 +1000

    move MemoryTransform into its own file

---
+++ src/main/scala/ir/transforms/MemoryTransform.scala
@@ -0,0 +1,42 @@
+package ir.transforms
+
+import analysis.data_structure_analysis.{IntervalGraph, isPlaceHolder}
+import ir.cilvisitor.{CILVisitor, ChangeTo, SkipChildren}
+import ir.{LocalAssign, LocalVar, MemoryAssign, MemoryLoad, MemoryStore, Procedure, Register, Statement}
+
+class MemoryTransform(dsa: Map[Procedure, IntervalGraph]) extends CILVisitor {
+  override def vstmt(e: Statement) = {
+    if dsa.contains(e.parent.parent) then
+      e match
+        case load: MemoryLoad =>
+          val indices = dsa(e.parent.parent).exprToCells(load.index)
+          if indices.nonEmpty then {
+            assert(indices.forall(_.hasPointee))
+            assert(indices.map(_.getPointee).size == 1, s"${indices.map(_.getPointee).size}, $load")
+            val value = indices.map(_.getPointee).head
+            val varName =  value.node.bases.keySet.filterNot(isPlaceHolder).toString() + value.interval
+            val rhs = if value.node.flags.global || value.node.flags.heap then
+              Register(varName, load.size)
+            else LocalVar(varName, load.lhs.getType)
+            val localAssign = LocalAssign(load.lhs, rhs, load.label)
+            ChangeTo(List(localAssign))
+          } else SkipChildren()
+        case store: MemoryStore =>
+          val indices = dsa(e.parent.parent).exprToCells(store.index)
+          if indices.nonEmpty then {
+            assert(indices.forall(_.hasPointee))
+            assert(indices.map(_.getPointee).size == 1)
+            val content = indices.map(_.getPointee).head
+            val varName =  content.node.bases.keySet.filterNot(isPlaceHolder).toString() + content.interval
+            val assign = if content.node.flags.global || content.node.flags.heap then
+              val lhs = Register(varName, store.size)
+              MemoryAssign(lhs, store.value, store.label)
+            else
+              val lhs = LocalVar(varName, store.value.getType)
+              LocalAssign(lhs, store.value, store.label)
+            ChangeTo(List(assign))
+          } else SkipChildren()
+        case _ => SkipChildren()
+    else SkipChildren()
+  }
+}
