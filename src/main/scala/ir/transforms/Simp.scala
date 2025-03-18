676e7b66b12a142b4f71323f92cb30967ec705f3
Sadra Bayat Tork <sbayattork@gmail.com>
2025-03-18 11:21:31 +1000

    refactor symBase isPlaceHolder

--- src/main/scala/ir/transforms/Simp.scala
+++ src/main/scala/ir/transforms/Simp.scala
@@ -11,8 +11,7 @@ import ir.*
 
 import scala.collection.mutable
 import analysis.*
-import analysis.data_structure_analysis.{Const, Heap, IntervalCell, IntervalGraph, Known, Loaded, Par, Ret, Stack, SymBase, Unknown}
-import ir.transforms.MemoryTransform.isPlaceHolder
+import analysis.data_structure_analysis.{Const, Heap, IntervalCell, IntervalGraph, Known, Loaded, Par, Ret, Stack, SymBase, Unknown, isPlaceHolder}
 
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration.*
@@ -1454,13 +1453,6 @@ class MemoryTransform(dsa: Map[Procedure, IntervalGraph]) extends CILVisitor {
   }
 }
 
-object MemoryTransform {
-  def isPlaceHolder(base: SymBase): Boolean = {
-    base match
-      case known: Known => false
-      case unknown: Unknown => true
-  }
-}
 
 
 
