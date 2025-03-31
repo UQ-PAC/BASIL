ea24a579ca7c9f49215875aeefd2901fca07dfdc
Sadra Bayat Tork <sbayattork@gmail.com>
2025-03-31 10:16:02 +1000

    transform local/global only memory

--- src/main/scala/ir/transforms/MemoryTransform.scala
+++ src/main/scala/ir/transforms/MemoryTransform.scala
@@ -1,57 +1,74 @@
 package ir.transforms
 
-import analysis.data_structure_analysis.{DSFlag, IntervalGraph, isPlaceHolder}
-import ir.cilvisitor.{CILVisitor, ChangeTo, SkipChildren}
-import ir.{LocalAssign, LocalVar, MemoryAssign, MemoryLoad, MemoryStore, Procedure, Register, Statement}
+import analysis.data_structure_analysis.{DSFlag, IntervalCell, IntervalGraph, isPlaceHolder}
+import ir.cilvisitor.{CILVisitor, ChangeTo, DoChildren, SkipChildren, VisitAction}
+import ir.{LocalAssign, LocalVar, Memory, MemoryAssign, MemoryLoad, MemoryStore, Procedure, Register, SharedMemory, Statement}
 
 class MemoryTransform(dsa: Map[Procedure, IntervalGraph]) extends CILVisitor {
+
+  def joinFlags(pointers: Iterable[IntervalCell]): DSFlag = {
+    val flag = DSFlag()
+    pointers.foreach(c => flag.join(c.node.flags))
+    flag
+  }
+
+  def cellsToName(cells: IntervalCell*): String = {
+    cells.flatMap(i =>
+        i.node.bases.keySet
+          .filterNot(isPlaceHolder)
+          .map(base => (base, i.node.get(i.interval).interval.move(f => f - i.node.bases(base))))
+      )
+      .mkString("|")
+  }
+
+  def isGlobal(flag: DSFlag): Boolean = {
+    flag.global && !flag.stack && !flag.heap
+  }
+
+  def isLocal(flag: DSFlag): Boolean = {
+    !flag.global && flag.stack && !flag.heap
+  }
+
   override def vstmt(e: Statement) = {
     if dsa.contains(e.parent.parent) then
       e match
         case load: MemoryLoad =>
-          val indices = dsa(e.parent.parent).exprToCells(load.index)
+          val indices = dsa(e.parent.parent).exprToCells(load.index).toSeq
           if indices.nonEmpty then {
             assert(indices.forall(_.hasPointee))
             assert(indices.map(_.getPointee).size == 1, s"${indices.map(_.getPointee).size}, $load")
-            val flag = DSFlag()
-            indices.foreach(c => flag.join(c.node.flags))
+            val flag = joinFlags(indices)
             val value = indices.map(_.getPointee).head
-            val varName = indices
-              .flatMap(i =>
-                i.node.bases.keySet
-                  .filterNot(isPlaceHolder)
-                  .map(base => (base, i.node.get(i.interval).interval.move(f => f - i.node.bases(base))))
-              )
-              .mkString("|")
-            val rhs =
-              if flag.global || flag.heap then Register(varName, load.size)
-              else LocalVar(varName, load.lhs.getType)
-            val localAssign = LocalAssign(load.lhs, rhs, load.label, false)
-            ChangeTo(List(localAssign))
+            val varName = cellsToName(indices:_*)
+
+            if isGlobal(flag) then
+              ChangeTo(List(LocalAssign(load.lhs, Register(varName, load.size), load.label)))
+            else if isLocal(flag) then
+              ChangeTo(List(LocalAssign(load.lhs, LocalVar(varName, load.lhs.getType), load.label)))
+            else
+              val newMem = SharedMemory(cellsToName(value), value.interval.size.getOrElse(0), load.mem.valueSize)
+              val newLoad = MemoryLoad(load.lhs, newMem, load.index, load.endian, load.size,  load.label)
+              ChangeTo(List(newLoad))
           } else SkipChildren()
+
+
         case store: MemoryStore =>
-          val indices = dsa(e.parent.parent).exprToCells(store.index)
+          val indices = dsa(e.parent.parent).exprToCells(store.index).toSeq
           if indices.nonEmpty then {
             assert(indices.forall(_.hasPointee))
             assert(indices.map(_.getPointee).size == 1)
             assert(indices.size == 1)
-            val flag = DSFlag()
-            indices.foreach(c => flag.join(c.node.flags))
+            val flag = joinFlags(indices)
             val content = indices.map(_.getPointee).head
-            val varName = indices
-              .flatMap(i =>
-                i.node.bases.keySet
-                  .filterNot(isPlaceHolder)
-                  .map(base => (base, i.node.get(i.interval).interval.move(f => f - i.node.bases(base))))
-              )
-              .mkString("|")
-            val assign = if flag.global || flag.heap then
-              val lhs = Register(varName, store.size)
-              MemoryAssign(lhs, store.value, store.label)
+            val varName = cellsToName(indices:_*)
+            if isGlobal(flag) then
+              ChangeTo(List(MemoryAssign(Register(varName, store.size), store.value, store.label)))
+            else if isLocal(flag) then
+              ChangeTo(List(LocalAssign(LocalVar(varName, store.value.getType), store.value, store.label, false)))
             else
-              val lhs = LocalVar(varName, store.value.getType)
-              LocalAssign(lhs, store.value, store.label, false)
-            ChangeTo(List(assign))
+              val newMem = SharedMemory(cellsToName(content), content.interval.size.getOrElse(0), store.mem.valueSize)
+              val newStore = MemoryStore(newMem, store.index, store.value, store.endian, store.size, store.label)
+              ChangeTo(List(newStore))
           } else SkipChildren()
         case _ => SkipChildren()
     else SkipChildren()
