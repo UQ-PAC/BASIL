1dcf940b461629e44701dc4339bd21e4bdaca1dc
Sadra Bayat Tork <sbayattork@gmail.com>
2025-03-31 12:02:27 +1000

    check for local unique pointers before changing to scalar

--- src/main/scala/ir/transforms/MemoryTransform.scala
+++ src/main/scala/ir/transforms/MemoryTransform.scala
@@ -1,10 +1,18 @@
 package ir.transforms
 
-import analysis.data_structure_analysis.{DSFlag, IntervalCell, IntervalGraph, isPlaceHolder}
+import analysis.data_structure_analysis.{DSFlag, IntervalCell, IntervalDSA, IntervalGraph, isPlaceHolder}
 import ir.cilvisitor.{CILVisitor, ChangeTo, DoChildren, SkipChildren, VisitAction}
 import ir.{LocalAssign, LocalVar, Memory, MemoryAssign, MemoryLoad, MemoryStore, Procedure, Register, SharedMemory, Statement}
 
 class MemoryTransform(dsa: Map[Procedure, IntervalGraph]) extends CILVisitor {
+
+  val revEdges = dsa.map((proc, graph) => (proc, IntervalDSA.getPointers(graph)))
+
+  def hasUniquePointer(cell: IntervalCell): Boolean = {
+    revEdges(cell.node.graph.proc).get(cell) match
+      case Some(value) =>  value.size == 1
+      case None => true // skipped due to assignment of non-address
+  }
 
   def joinFlags(pointers: Iterable[IntervalCell]): DSFlag = {
     val flag = DSFlag()
@@ -30,7 +38,8 @@ class MemoryTransform(dsa: Map[Procedure, IntervalGraph]) extends CILVisitor {
   }
 
   override def vstmt(e: Statement) = {
-    if dsa.contains(e.parent.parent) then
+    val proc = e.parent.parent
+    if dsa.contains(proc) then
       e match
         case load: MemoryLoad =>
           val indices = dsa(e.parent.parent).exprToCells(load.index).toSeq
@@ -41,9 +50,9 @@ class MemoryTransform(dsa: Map[Procedure, IntervalGraph]) extends CILVisitor {
             val value = indices.map(_.getPointee).head
             val varName = cellsToName(indices:_*)
 
-            if isGlobal(flag) then
+            if isGlobal(flag) && hasUniquePointer(value) then
               ChangeTo(List(LocalAssign(load.lhs, Register(varName, load.size), load.label)))
-            else if isLocal(flag) then
+            else if isLocal(flag) && hasUniquePointer(value) then
               ChangeTo(List(LocalAssign(load.lhs, LocalVar(varName, load.lhs.getType), load.label)))
             else
               val newMem = SharedMemory(cellsToName(value), value.interval.size.getOrElse(0), load.mem.valueSize)
@@ -61,9 +70,9 @@ class MemoryTransform(dsa: Map[Procedure, IntervalGraph]) extends CILVisitor {
             val flag = joinFlags(indices)
             val content = indices.map(_.getPointee).head
             val varName = cellsToName(indices:_*)
-            if isGlobal(flag) then
+            if isGlobal(flag) && hasUniquePointer(content) then
               ChangeTo(List(MemoryAssign(Register(varName, store.size), store.value, store.label)))
-            else if isLocal(flag) then
+            else if isLocal(flag) && hasUniquePointer(content)then
               ChangeTo(List(LocalAssign(LocalVar(varName, store.value.getType), store.value, store.label, false)))
             else
               val newMem = SharedMemory(cellsToName(content), content.interval.size.getOrElse(0), store.mem.valueSize)
