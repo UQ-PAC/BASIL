171754af2904ebf375b9cfc04da52d283fe3008d
Kait <39479354+katrinafyi@users.noreply.github.com>
2025-04-17 15:41:07 +1000

    runtime evaluation of Scala code (#404)
    * runtime evaluation of Scala code
    
    This adds some methods to support the evaluation of Scala code at
    runtime, with the main goal of evaluating Basil DSL strings such as
    those produced by ToScala.
    
    This is a somewhat hacky way to close the loop of Basil IR
    representations and allows dumping and re-loading of Basil IR to/from
    disk.
    
    This is implemented by adding a dependency on the scala3-compiler
    package, so we can access its script engine through the Java
    ScriptEngine interface:
    https://docs.oracle.com/javase/8/docs/api/javax/script/ScriptEngine.html
    
    * rename Eval to EvalScala
    
    * update comment

---
+++ src/test/scala/EvalScalaTest.scala
@@ -0,0 +1,63 @@
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import scala.util.{Try, Failure, Success}
+import java.io.OutputStream
+
+import ir.*
+import ir.dsl.*
+
+@test_util.tags.UnitTest
+class EvalScalaTest extends AnyFunSuite with test_util.CaptureOutput with Matchers {
+
+  import util.EvalScala.EvalResult
+
+  import ir.dsl.{given}
+
+  val program: Program = prog(
+    proc(
+      "main",
+      block(
+        "first_call",
+        LocalAssign(R0, bv64(1), None),
+        LocalAssign(R1, bv64(1), None),
+        directCall("callee1"),
+        goto("second_call")
+      ),
+      block("second_call", directCall("callee2"), goto("returnBlock")),
+      block("returnBlock", ret)
+    ),
+    proc("callee1", block("returnBlock", ret)),
+    proc("callee2", block("returnBlock", ret)),
+    proc("empty procedure")
+  )
+
+  test("basic eval") {
+    assertResult(Success(10)) { util.EvalScala.eval("1+9").result }
+    assertResult(Success("asd")) { util.EvalScala.eval("\"asd\"").result }
+    assertResult(Success(List(1, 2, 3))) { util.EvalScala.eval("scala.collection.immutable.Seq(1, 2, 3)").result }
+  }
+
+  test("eval runtime failures") {
+    // WARN: these tests rely on the possibly-unreliable unwrapScalaEvalException method.
+    // if they start failing because the returned exceptions differ from the expected types,
+    // check that method.
+
+    util.EvalScala.eval("1 match { case 2 => () }").failure should matchPattern { case _: MatchError => }
+    util.EvalScala.eval("throw new Exception(\"boop\")").failure should matchPattern {
+      case x: Exception if x.getMessage == "boop" =>
+    }
+    util.EvalScala.eval("1 / 0").failure should matchPattern { case _: ArithmeticException => }
+    util.EvalScala.eval("1 / 0\n1+2").failure should matchPattern { case _: ArithmeticException => }
+  }
+
+  test("eval compile failures") {
+    util.EvalScala.eval("1+") should matchPattern { case EvalResult(Failure(_), s) if s != "" => }
+    util.EvalScala.eval("undefinedvariable") should matchPattern { case EvalResult(Failure(_), s) if s != "" => }
+    util.EvalScala.eval(".") should matchPattern { case EvalResult(Failure(_), s) if s != "" => }
+  }
+
+  test("eval of toscala") {
+    val p2 = util.EvalScala.evalDSL(program.toScala).get
+    assertResult(program.toString) { p2.toString }
+  }
+}
