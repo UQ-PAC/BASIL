1308e041c0e02b6d5b19cb804d11051b1d893050
Alistair Michael <alistair.michael@uq.edu.au>
2025-05-06 11:17:18 +1000

    partial flow sensitivity

--- src/main/scala/ir/transforms/Simp.scala
+++ src/main/scala/ir/transforms/Simp.scala
@@ -1188,6 +1188,8 @@ object OffsetProp {
   class CopyProp() {
     val st = mutable.Map[Variable, Value]()
     var giveUp = false
+    val lastUpdate = mutable.Map[Block, Int]()
+    var stSequenceNo = 1
 
     def findOff(v: Variable, c: BitVecLiteral): BitVecLiteral | Variable | BinaryExpr = find(v) match {
       case lc: BitVecLiteral => ir.eval.BitVectorEval.smt_bvadd(lc, c)
@@ -1209,7 +1211,12 @@ object OffsetProp {
 
     def joinState(lhs: Variable, rhs: Expr) = {
       specJoinState(lhs, rhs) match {
-        case Some((l, r)) => st(l) = r
+        case Some((l, r)) => {
+          if (st.contains(l) && st(l) != r) {
+            stSequenceNo += 1
+          }
+          st(l) = r
+        }
         case _ => ()
       }
     }
@@ -1256,10 +1263,17 @@ object OffsetProp {
     def analyse(p: Procedure): Map[Variable, Expr] = {
       reversePostOrder(p)
       val worklist = mutable.PriorityQueue[Block]()(Ordering.by(_.rpoOrder))
-      worklist.addAll(p.blocks)
+      worklist.addAll(p.entryBlock)
       while (worklist.nonEmpty && !giveUp) {
         val b = worklist.dequeue()
+        val seq = lastUpdate.get(b).getOrElse(0)
+
         b.statements.foreach(transfer)
+
+        if (stSequenceNo != seq || seq == 0) {
+          lastUpdate(b) = stSequenceNo
+          worklist.addAll(b.nextBlocks)
+        }
       }
 
       val res: Map[Variable, Variable | Literal | BinaryExpr] =
@@ -1301,6 +1315,8 @@ object MinCopyProp {
 
   class CopyProp() {
     val st = mutable.Map[Variable, Value]()
+    val lastUpdate = mutable.Map[Block, Int]()
+    var stSequenceNo = 1
     var giveUp = false
 
     def find(v: Variable): Literal | Variable = {
@@ -1331,7 +1347,12 @@ object MinCopyProp {
 
     def joinState(lhs: Variable, rhs: Variable | Literal) = {
       specJoinState(lhs, rhs) match {
-        case Some((l, r)) => st(l) = r
+        case Some((l, r)) => {
+          if (st.contains(l) && st(l) != r) {
+            stSequenceNo += 1
+          }
+          st(l) = r
+        }
         case _ => ()
       }
     }
@@ -1364,10 +1385,17 @@ object MinCopyProp {
     def analyse(p: Procedure): Map[Variable, Variable | Literal] = {
       reversePostOrder(p)
       val worklist = mutable.PriorityQueue[Block]()(Ordering.by(_.rpoOrder))
-      worklist.addAll(p.blocks)
+      worklist.addAll(p.entryBlock)
       while (worklist.nonEmpty && !giveUp) {
         val b = worklist.dequeue()
+        val seq = lastUpdate.get(b).getOrElse(0)
+
         b.statements.foreach(transfer)
+
+        if (stSequenceNo != seq || seq == 0) {
+          lastUpdate(b) = stSequenceNo
+          worklist.addAll(b.nextBlocks)
+        }
       }
 
       val res: Map[Variable, Variable | Literal] =
