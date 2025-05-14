fae4e28e2f81cdcac65c88fb9e64706ca367c86d
Sadra Bayat Tork <sbayattork@gmail.com>
2025-05-14 15:22:10 +1000

    remove unused functions from memory transform

--- src/main/scala/ir/transforms/MemoryTransform.scala
+++ src/main/scala/ir/transforms/MemoryTransform.scala
@@ -14,59 +14,6 @@ class MemoryTransform(dsa: Map[Procedure, IntervalGraph], globals: Map[IntervalN
 //    dsa.map((proc, graph) => (proc, IntervalDSA.getPointers(graph)))
   val interProcCells: Map[IntervalCell, Set[IntervalCell]] = Map.empty // computeRelations()
 
-  def computeRelations(): Map[IntervalCell, Set[IntervalCell]] = {
-    val cellMapping = mutable.Map[IntervalCell, Set[IntervalCell]]()
-    dsa.foreach((proc, graph) =>
-      val callerDSG = dsa(proc)
-      proc.foreach {
-        case dc: DirectCall if dsa.contains(dc.target) =>
-          val calleeDSG = dsa(dc.target)
-          dc.actualParams.foreach((formal, actual) =>
-            val cells1 = callerDSG.exprToCells(actual)
-            val cells2 = calleeDSG.exprToCells(formal)
-            cells1.foreach(cell => cellMapping.update(cell, cellMapping.getOrElse(cell, Set.empty) ++ cells2))
-            cells2.foreach(cell => cellMapping.update(cell, cellMapping.getOrElse(cell, Set.empty) ++ cells1))
-          )
-          dc.outParams.foreach((formal, actual) =>
-            val cells1 = callerDSG.exprToCells(actual)
-            val cells2 = calleeDSG.exprToCells(formal)
-            cells1.foreach(cell => cellMapping.update(cell, cellMapping.getOrElse(cell, Set.empty) ++ cells2))
-            cells2.foreach(cell => cellMapping.update(cell, cellMapping.getOrElse(cell, Set.empty) ++ cells1))
-          )
-        case _ =>
-      }
-    )
-    cellMapping.toMap
-  }
-
-  private def getCorrespondingGlobals(cell: IntervalCell) = {
-    if cell.node.flags.global then dsa.values.map(g => g.find(g.nodes(Global)).get(cell.interval)).toSet
-    else Set.empty
-  }
-
-  def hasUniquePointer(cell: IntervalCell): Boolean = {
-    val proc = cell.node.graph.proc
-    revEdges(proc).get(cell) match
-      case Some(value) if value.size > 1 => false
-      case v @ _ if interProcCells.contains(cell) || getCorrespondingGlobals(cell).size > 1 =>
-        var seenPointers = v.getOrElse(Set.empty)
-        var seenCells = Set(cell)
-        val queue = mutable.Queue().enqueueAll(interProcCells.getOrElse(cell, Set.empty))
-        queue.enqueueAll(getCorrespondingGlobals(cell).diff(seenCells))
-        while queue.nonEmpty && seenPointers.size <= 1 do
-          val eq = queue.dequeue()
-          seenCells += cell
-          val pointers = revEdges(eq.node.graph.proc)
-            .getOrElse(eq, Set.empty)
-            .diff(seenPointers)
-            .filterNot(p => interProcCells.getOrElse(p, Set.empty).exists(seenPointers.contains))
-          seenPointers = seenPointers.union(pointers)
-
-        seenPointers.size <= 1
-      case _ => true
-
-  }
-
   def isGlobal(flag: DSFlag): Boolean = {
     flag.global && !flag.stack && !flag.heap
   }
