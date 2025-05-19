4069d65a1c95bac7d67dfc52b481102a962c725b
Sadra Bayat Tork <sbayattork@gmail.com>
2025-05-19 15:55:19 +1000

    clone eq classes

--- src/main/scala/analysis/data_structure_analysis/IntervalDSA.scala
+++ src/main/scala/analysis/data_structure_analysis/IntervalDSA.scala
@@ -614,6 +614,7 @@ class IntervalNode(
         val v = IntervalNode(newGraph, node.bases, node.size)
         v.flags.join(node.flags)
         node.cells.foreach(cell => v.add(cell.interval))
+        v.eqClasses = node.eqClasses.map(eq => eq.map(cell => IntervalCell(v, cell.interval)))
         oldToNew.update(node, v)
         v
       else oldToNew(node)
