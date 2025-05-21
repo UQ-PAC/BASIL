00b8376136ca8db59965fdb760b4c62c87e4b6e7
Sadra Bayat Tork <sbayattork@gmail.com>
2025-05-21 12:50:29 +1000

    make config consistent

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -225,6 +225,8 @@ object Main {
     dsaSplitGlobals: Flag,
     @arg(name = "eq-cells", doc = "allow cells from same node to be merged without collapsing (requires --dsa (local|bu|td)")
     dsaEqCells: Flag,
+    @arg(name = "dsa-assert", doc = "insert assertions to check globals offset to top fall within global region bounds")
+    dsaAssert: Flag,
     @arg(name = "memory-transform", doc = "Transform memory access to region accesses")
     memoryTransform: Flag,
     @arg(name = "noif", doc = "Disable information flow security transform in Boogie output")
@@ -309,7 +311,8 @@ object Main {
       Some(DSConfig(
         phase,
         conf.dsaSplitGlobals.value,
-        !conf.dsaEqCells.value,
+        conf.dsaAssert.value,
+        conf.dsaEqCells.value,
         conf.dsaChecks.value
       ))
     } else {
