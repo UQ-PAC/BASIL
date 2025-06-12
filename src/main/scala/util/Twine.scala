7c366e0a5adcf49fcdbe91886f14a058924a8b23
rina <k@rina.fyi>
2025-06-12 12:53:58 +1000

    docs

--- src/main/scala/util/Twine.scala
+++ src/main/scala/util/Twine.scala
@@ -1,10 +1,27 @@
 package util.twine
 
+/**
+ * A [[Twine]] is a tree representation of a string.
+ *
+ * This lets us operate on strings without constantly needing to re-create
+ * and copy entirely new strings. Using the subclasses within this trait,
+ * we can do operations like concatenate and indent very efficiently.
+ *
+ * The Twine is only converted to a string once, using [[Twine#mkString]],
+ * when all operations are done and the final string needs to be emitted.
+ */
 sealed trait Twine {
 
+  /**
+   * Formats the current Twine into a string.
+   */
   def mkString: String =
     build(StringBuilder()).result()
 
+  /**
+   * Appends the current Twine into the given [[StringBuilder]], with optional
+   * additional options.
+   */
   def build(sb: StringBuilder, indent: String = "  ", newline: String = System.lineSeparator()): StringBuilder = {
 
     // we need to insert newlines /between/ elements of Lines.
@@ -15,12 +32,13 @@ sealed trait Twine {
 
     def helper(tw: Twine, ind: String): Unit = tw match {
       case Indent(tw) => helper(tw, ind + indent)
-      case Str(s) =>
+      case Str(s) if s.nonEmpty =>
         if (doNewline)
           doNewline = false
           sb ++= newline
           sb ++= ind
         sb ++= s
+      case Str(_) => ()
       case Lines(lines) =>
         var first = true
         lines.foreach { case l =>
@@ -39,21 +57,62 @@ sealed trait Twine {
   def +:(s: String) = Twine(s, this)
 }
 
+/**
+ * Indents each line within the given Twine. Lines are those defined by [[Lines]].
+ *
+ * Indent width is specified later, when the Twine is eventually converted
+ * to a string.
+ */
 case class Indent(tw: Twine) extends Twine
+
+/**
+ * A simple string component. This must not contain any newline characters,
+ * either [[Lines]] or [[Twine.lines]] must be used for building multi-line Twines.
+ */
 case class Str(s: String) extends Twine {
+  require(!s.contains('\n'), "Twine's Str constructor cannot be used for multi-line strings. Use Lines instead.")
+
+  /**
+   * Overrides the case class's toString to display the contained string
+   * with quotes and escapes (i.e., like a Scala string literal).
+   */
   override def toString = s"Str(${util.StringEscape.quote(s)})"
 }
+
+/**
+ * Multiple lines. Each component of the given iterable is joined by a newline
+ * (and any necessary indentation). Newlines are used as *separators*, not terminators.
+ */
 case class Lines(ss: Iterable[Twine]) extends Twine
+
+/**
+ * Concatenates the given twines, placing them directly adjacent to each other.
+ */
 case class Concat(tws: Iterable[Twine]) extends Twine
 
+/**
+ * Provides useful methods for constructing nd manipulating Twines
+ * (e.g., [[Twine.empty]], [[Twine.lines]], [[Twine.indentNested]]).
+ * Can be used with the [[Twine.apply]] method to convert a number of strings to a Twine and concatenate
+ * them.
+ */
 object Twine {
+  /**
+   * The empty twine.
+   */
   val empty = Concat(Nil)
 
+  /**
+   * Coerces a String to a Twine, or returns the given Twine.
+   */
   def apply(x: String | Twine): Twine = x match {
     case x: String => Str(x)
     case x: Twine => x
   }
 
+  /**
+   * Concatenates the given parts into a Twine.
+   */
   def apply(parts: Seq[String | Twine]): Twine = {
     if (parts.isEmpty) {
       empty
@@ -64,13 +123,23 @@ object Twine {
     }
   }
 
+  /**
+   * Concatenates the given parts into a Twine.
+   */
   @scala.annotation.targetName("applyMany")
   def apply(parts: (String | Twine)*): Twine =
     apply(parts)
 
+  /**
+   * Indents the given twine.
+   */
   def indent(tw: Twine) = Indent(tw)
 
-  def lines(parts: (String | Twine)*): Twine =
+  /**
+   * Joins the given parts into a multi-line Twine,
+   * separated by line separators.
+   */
+  def lines(parts: (String | Twine)*) =
     Lines(parts.map(Twine(_)))
 
   /**
@@ -79,25 +148,33 @@ object Twine {
   *
   * The produced string will follow approximately this pattern:
   *
-  *     head newline INDENT( elem1 sep newline elem2 sep newline elem3 ...) newline tail
+  * ```
+  * head newline INDENT( elem1 sep newline elem2 sep newline elem3 ...) newline tail
+  * ```
   *
   * where INDENT(...) indicates that the ... is to be indented. With the default `sep`
   * and `newline`, this will produce something like this:
   *
-  *     head
-  *       elem1,
-  *       elem2,
-  *       elem3
-  *     tail
+  * ```
+  * head
+  *   elem1,
+  *   elem2,
+  *   elem3
+  * tail
+  * ```
   *
   * If `headSep` is set, the function will place a separator after `head`
   * and before the first newline, i.e.:
   *
-  *     head sep newline INDENT( elem1 sep newline elem2 ...) newline tail
+  * ```
+  * head sep newline INDENT( elem1 sep newline elem2 ...) newline tail
+  * ```
   *
   * In all cases, if `elems` is empty, no newlines will be inserted and the returned twine is simply
   *
-  *     head tail
+  * ```
+  * head tail
+  * ```
   */
   def indentNested(
     head: String,
