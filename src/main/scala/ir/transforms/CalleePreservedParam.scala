a64520ea8fc0eab146640afc3b934ac302db1e06
alistair <alistair.michael@uq.edu.au>
2025-06-12 22:10:39 +1000

    cleanup

--- src/main/scala/ir/transforms/CalleePreservedParam.scala
+++ src/main/scala/ir/transforms/CalleePreservedParam.scala
@@ -9,13 +9,16 @@ object CalleePreservedParam {
    * Asusming single-return and parameter form 
    */
 
-  val preserved = (19 to 29).map(i => s"R$i").toSet
+  val preserved = (19 to 29).map(i => s"R$i").toSet // required by ABI
+  val callReturnStackPreserved = Set("R30", "R31") // not strictly required
 
   def isPreservedParam(v: LocalVar) = {
     // abi assumption
     v.varName match {
-      case "R29_in" | "R30_in" | "R31_in" | "R29" | "R30" | "R31" | "R29_out" | "R30_out" | "R31_out" => true
-      case o if preserved.contains(o.stripSuffix("_in").stripSuffix("_out")) => true
+      case s"${reg}_in" => callReturnStackPreserved.contains(reg)
+      case s"${reg}_out" => callReturnStackPreserved.contains(reg)
+      case s"${reg}_in" => preserved.contains(reg)
+      case s"${reg}_out" => preserved.contains(reg)
       case _ => false
     }
   }
@@ -32,19 +35,23 @@ object CalleePreservedParam {
 
       for (param <- p.formalOutParam.filter(isPreservedParam)) {
 
-        val input = p.formalInParam.find(_.name.startsWith(param.varName.stripSuffix("_out"))).get
+        val input = p.formalInParam.collect {
+          case p @ LocalVar(s"${reg}_in", _, _) if reg == param.name.stripSuffix("_out") => p
+        }.head
+        // we don't expect it to be possible for these params to be removed from the param list
+        // since they are preserved across calls they must be live at entry
 
         p.formalOutParam.remove(param)
 
         for (call <- p.incomingCalls()) {
-          call.outParams = call.outParams.removed(param)
+          call.outParams = call.outParams - param
         }
 
         p.returnBlock.foreach(b => {
           val out = b.jump match {
             case r: Return => {
               val arg = r.outParams(param)
-              r.outParams = r.outParams.removed(param)
+              r.outParams = r.outParams - param
               arg
             }
             case o => ???
