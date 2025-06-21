b99373ebc725ab8a2361310a09bc6d980f3332a2
rina <k@rina.fyi>
2025-06-21 15:01:34 +1000

    rename

--- src/main/scala/gtirb/AuxDecoder.scala
+++ src/main/scala/gtirb/AuxDecoder.scala
@@ -10,13 +10,13 @@ import com.grammatech.gtirb.proto.AuxData.AuxData
 object AuxDecoder {
 
   type Input = ByteArrayInputStream
-  type Reader[T] = Input => T
+  type Decoder[T] = Input => T
 
-  def decode[T](reader: Reader[T])(bytes: ByteString): T =
-    reader(ByteArrayInputStream(bytes.toByteArray))
+  def decode[T](decoder: Decoder[T])(bytes: ByteString): T =
+    decoder(ByteArrayInputStream(bytes.toByteArray))
 
-  def decode[T](reader: Reader[T])(aux: AuxData): T =
-    reader(ByteArrayInputStream(aux.data.toByteArray))
+  def decode[T](decoder: Decoder[T])(aux: AuxData): T =
+    decoder(ByteArrayInputStream(aux.data.toByteArray))
 
   def readBytes(numBytes: Int)(bs: Input): Array[Byte] =
     val bytes = bs.readNBytes(numBytes)
@@ -42,41 +42,41 @@ object AuxDecoder {
       acc * 256 + (if (!signed && n < 0) then n + 256 else n)
     }
 
-  def readMap[K, V](keyReader: Reader[K], valReader: Reader[V])(bs: Input) =
+  def readMap[K, V](keyDecoder: Decoder[K], valDecoder: Decoder[V])(bs: Input) =
     val len = readUint(64)(bs)
     (BigInt(0) until len).map { case _ =>
-      val k = keyReader(bs)
-      val v = valReader(bs)
+      val k = keyDecoder(bs)
+      val v = valDecoder(bs)
       k -> v
     }.toMap
 
-  def readSet[K, V](valReader: Reader[V])(bs: Input) =
+  def readSet[K, V](valDecoder: Decoder[V])(bs: Input) =
     val len = readUint(64)(bs)
-    (BigInt(0) until len).map(_ => valReader(bs)).toSet
+    (BigInt(0) until len).map(_ => valDecoder(bs)).toSet
 
-  def readList[K, V](valReader: Reader[V])(bs: Input) =
+  def readList[K, V](valDecoder: Decoder[V])(bs: Input) =
     val len = readUint(64)(bs)
-    (BigInt(0) until len).map(_ => valReader(bs)).toList
+    (BigInt(0) until len).map(_ => valDecoder(bs)).toList
 
-  def readTuple[T1, T2](r1: Reader[T1], r2: Reader[T2])(bs: Input) =
+  def readTuple[T1, T2](r1: Decoder[T1], r2: Decoder[T2])(bs: Input) =
     val x1 = r1(bs)
     val x2 = r2(bs)
     (x1, x2)
 
-  def readTuple[T1, T2, T3](r1: Reader[T1], r2: Reader[T2], r3: Reader[T3])(bs: Input) =
+  def readTuple[T1, T2, T3](r1: Decoder[T1], r2: Decoder[T2], r3: Decoder[T3])(bs: Input) =
     val x1 = r1(bs)
     val x2 = r2(bs)
     val x3 = r3(bs)
     (x1, x2, x3)
 
-  def readTuple[T1, T2, T3, T4](r1: Reader[T1], r2: Reader[T2], r3: Reader[T3], r4: Reader[T4])(bs: Input) =
+  def readTuple[T1, T2, T3, T4](r1: Decoder[T1], r2: Decoder[T2], r3: Decoder[T3], r4: Decoder[T4])(bs: Input) =
     val x1 = r1(bs)
     val x2 = r2(bs)
     val x3 = r3(bs)
     val x4 = r4(bs)
     (x1, x2, x3, x4)
 
-  def readTuple[T1, T2, T3, T4, T5](r1: Reader[T1], r2: Reader[T2], r3: Reader[T3], r4: Reader[T4], r5: Reader[T5])(
+  def readTuple[T1, T2, T3, T4, T5](r1: Decoder[T1], r2: Decoder[T2], r3: Decoder[T3], r4: Decoder[T4], r5: Decoder[T5])(
     bs: Input
   ) =
     val x1 = r1(bs)
@@ -87,12 +87,12 @@ object AuxDecoder {
     (x1, x2, x3, x4, x5)
 
   def readTuple[T1, T2, T3, T4, T5, T6](
-    r1: Reader[T1],
-    r2: Reader[T2],
-    r3: Reader[T3],
-    r4: Reader[T4],
-    r5: Reader[T5],
-    r6: Reader[T6]
+    r1: Decoder[T1],
+    r2: Decoder[T2],
+    r3: Decoder[T3],
+    r4: Decoder[T4],
+    r5: Decoder[T5],
+    r6: Decoder[T6]
   )(bs: Input) =
     val x1 = r1(bs)
     val x2 = r2(bs)
