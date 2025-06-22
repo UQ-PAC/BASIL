82b8710132fd42d667444cb9501b0246858b2416
am <alistair.michael@uq.edu.au>
2025-06-22 16:24:56 +1000

    docs

--- src/main/scala/util/assert/Assert.scala
+++ src/main/scala/util/assert/Assert.scala
@@ -2,18 +2,36 @@ package util.assertion
 
 import basil.compileConstants.debugMode
 
-var disableAssetions = false
+var disableAssertions = false
 
 /*
- * Conditionally inlined assertion that should not allocate anything
+ * An assertion that can be disabled at run-time --nodebug flag, or compile-time.
  */
 inline def debugAssert(inline assertion: Boolean, inline message: String*): Unit = {
+  // Conditionally inlined assertion and should not allocate anything except in evaluation or failure of [[assertion]]
   inline if (debugMode) then {
-    if (!disableAssetions && !assertion) then
+    if (!disableAssertions && !assertion) then
       if (message.nonEmpty) then {
         throw AssertionError(message.mkString(""))
       } else {
         throw AssertionError()
+      }
+  } else {
+    ()
+  }
+}
+
+
+/*
+ * Like (assert) but with a slightly different message. Shadows's scalas built-in require function..
+ */
+inline def require(inline assertion: Boolean, inline message: String*): Unit = {
+  inline if (debugMode) then {
+    if (!disableAssertions && !assertion) then
+      if (message.nonEmpty) then {
+        throw AssertionError("Requirement failed: " + message.mkString(""))
+      } else {
+        throw AssertionError("Requirement failed.")
       }
   } else {
     ()
