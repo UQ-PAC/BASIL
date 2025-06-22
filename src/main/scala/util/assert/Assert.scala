94edf5a9217366c833517f0c2043040e1f1271ae
am <alistair.michael@uq.edu.au>
2025-06-22 13:22:00 +1000

    fix

--- src/main/scala/util/assert/Assert.scala
+++ src/main/scala/util/assert/Assert.scala
@@ -2,17 +2,20 @@ package util.assertion
 
 import basil.compileConstants.debugMode
 
+var disableAssetions = false
+
 /*
  * Conditionally inlined assertion that should not allocate anything
  */
 inline def assert(inline assertion: Boolean, inline message: String*): Unit = {
-  inline if (debugMode) then ()
-  else {
-    if (!assertion) then
+  inline if (debugMode) then {
+    if (!disableAssetions && !assertion) then
       if (message.nonEmpty) then {
         throw AssertionError(message.mkString(""))
       } else {
         throw AssertionError()
       }
+  } else {
+    ()
   }
 }
