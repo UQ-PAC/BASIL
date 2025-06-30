34dd4232e14b303e1c86c7fdd6a5200e6953be7c
Alistair Michael <alistair.michael@uq.edu.au>
2025-06-30 15:08:56 +1000

    fix match exhaustivity warns

--- src/main/scala/analysis/data_structure_analysis/SymbolicValueAnalysis.scala
+++ src/main/scala/analysis/data_structure_analysis/SymbolicValueAnalysis.scala
@@ -394,8 +394,9 @@ class SymValuesDomain[T <: Offsets](using symValSetDomain: SymValSetDomain[T]) e
     val block = b.parent
     b match
       case SimulAssign(assignments, _) =>
-        val update = assignments.map { case (lhs: LocalVar, rhs) =>
-          lhs -> SymValues.exprToSymValSet(a)(rhs)
+        val update = assignments.map { 
+          case (lhs: LocalVar, rhs) => lhs -> SymValues.exprToSymValSet(a)(rhs)
+          case (lhs: GlobalVar, _) => throw Exception("GlobalVar on lhs of SimulAssign not expected")
         }.toMap
         join(a, SymValues(update), block)
       case LocalAssign(lhs: LocalVar, rhs: Expr, _) =>
