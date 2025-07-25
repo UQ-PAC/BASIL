34d0ccb2ce40e03b85ef9934e211de0b689d1a54
Alexander Arthur <legsoflemurs@outlook.com>
2025-07-25 14:36:44 +1000

    Overly simplistic tests of tnum-interval reduction logic

---
+++ src/test/scala/TNumIntervalReducedProductTest.scala
@@ -0,0 +1,42 @@
+import analysis.Interval.ConcreteInterval
+import analysis.{TNum, TNumIntervalReducedProduct}
+import ir.BitVecLiteral
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class TNumIntervalReducedProductTest extends AnyFunSuiteLike {
+
+  val domain = TNumIntervalReducedProduct()
+  val interval = ConcreteInterval(6, 10, 4)
+  val tnum = TNum(BitVecLiteral(0,4), BitVecLiteral(9, 4))
+
+  test("Refine lower bound - simple example") {
+    val expected = 8
+    assert(expected == domain.refineLowerBound(6, tnum))
+  }
+
+  test("Refine lower bound - 0") {
+    val expected = 0
+    assert(expected == domain.refineLowerBound(0, tnum))
+  }
+
+  test("Refine upper bound - simple example") {
+    val expected = 9
+    assert(expected == domain.refineUpperBound(10, tnum))
+  }
+
+  test("Refine upper bound - 0") {
+    val expected = 0
+    assert(expected == domain.refineUpperBound(0, tnum))
+  }
+
+  test("Refine tnum - simple example") {
+    val expected = TNum(BitVecLiteral(8, 4), BitVecLiteral(1, 4))
+    assert(expected == domain.refineTnum(8, 9, tnum))
+  }
+
+  test("Refine tnum - 0") {
+    val expected = TNum(BitVecLiteral(0, 4), BitVecLiteral(0, 4))
+    assert(expected == domain.refineTnum(0, 0, tnum))
+  }
+
+}
