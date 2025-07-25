6472372f183e3d2d03708cf3492babf6aadf88c3
pragu <pragun.banga@gmail.com>
2025-07-25 15:55:51 +1000

    Map formal out params of procedures to abstract states

--- src/main/scala/analysis/InterCopyConst.scala
+++ src/main/scala/analysis/InterCopyConst.scala
@@ -11,29 +11,26 @@ trait CopyConstAnalysisFunctions(parameterForm: Boolean) extends ForwardIDEAnaly
 
   def edgesCallToEntry(call: DirectCall, entry: Procedure)(d: DL): Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]] = {
 
-  //print("actual params: " + call.actualParams)
 
-
-    // below only for param form no?, otherwise just give everything
     if !parameterForm then Map(d->IdEdge())
     else
       d match {
-        case Left(a) =>
+        case Left(a) => // already existing variables
+
           call.actualParams.toList.foldLeft(Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]]()) {
            case (m, (inVar, expression)) => expression match
-             case LocalVar(_, _, _) | Register(_, _) if expression == a => m ++ Map(Left(inVar) -> IdEdge(), d -> IdEdge()) // idk if this actually checks properly
+             case LocalVar(_, _, _) | Register(_, _) if expression == a => m ++ Map(Left(inVar) -> IdEdge(), d -> IdEdge())
              case LocalVar(_, _, _) | Register(_, _) if expression != a && inVar != a => m ++ Map(d -> IdEdge())
              case _ => m ++ Map()
 
           }
         case Right(a) =>
           val lambdaToLambda: Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]] = Map(d -> IdEdge())
-          call.actualParams.toList.foldLeft(Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]]()) {
+            call.actualParams.toList.foldLeft(Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]]()) {
             case (m, (inVar, expression)) => expression match {
               case LocalVar(_,_,_) | Register(_,_) => m ++ lambdaToLambda //not add anything else from lambda
-              case BitVecLiteral(value, size) => m ++ lambdaToLambda ++ Map(Left(inVar)->ConstEdge(valuelattice.bv(BitVecLiteral(value, size))))
+              case BitVecLiteral(value, size) => m ++ lambdaToLambda ++ Map(Left(inVar)->ConstEdge(valuelattice.bv(BitVecLiteral(value, size)))) //assign val to in param
               case _ => m ++ lambdaToLambda ++ Map(Left(inVar) -> ConstEdge(valuelattice.top))
-              // direct call?
 
             }
 
@@ -47,7 +44,6 @@ trait CopyConstAnalysisFunctions(parameterForm: Boolean) extends ForwardIDEAnaly
 
   def edgesExitToAfterCall(exit: Return, aftercall: Command)(d: DL): Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]] = {
 
-    //print(d)
 
     if !parameterForm then Map(d->IdEdge())
     else
@@ -61,7 +57,6 @@ trait CopyConstAnalysisFunctions(parameterForm: Boolean) extends ForwardIDEAnaly
           exit.outParams.toList.foldLeft(Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]]()) {
             case (m, (retVar, expression)) => expression match
               case LocalVar(_, _, _) | Register(_, _) if expression == a => m ++ Map(Left(call.outParams(retVar)) -> IdEdge())
-              //case LocalVar(_, _, _) | Register(_, _) if expression != a => m ++ Map(d -> IdEdge()) // lol need to fix up here in params shld always just be Map() so add case
               case _ => m ++ Map() //ignore other kind of expr, including local vars / in params of the procedure
 
           }
@@ -74,6 +69,7 @@ trait CopyConstAnalysisFunctions(parameterForm: Boolean) extends ForwardIDEAnaly
               case BitVecLiteral(value, size) => m ++ lambdaToLambda ++ Map(Left(call.outParams(retVar)) -> ConstEdge(valuelattice.bv(BitVecLiteral(value, size))))
               case _ => m ++ lambdaToLambda ++ Map(Left(call.outParams(retVar)) -> ConstEdge(valuelattice.top))
 
+
             }
 
           }
@@ -85,13 +81,12 @@ trait CopyConstAnalysisFunctions(parameterForm: Boolean) extends ForwardIDEAnaly
   def edgesCallToAfterCall(call: DirectCall, aftercall: Command)(d: DL): Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]] = {
 
     if !parameterForm then Map()
-    else  // unused locals in function and lambda need identity
+    else
       d match {
-        case Left(v) if (call.outParams.exists(_._2 == v)  || call.actualParams.exists(_._2 == v)) => Map()
-        case _ => Map(d->IdEdge())
+        case Left(v) if call.outParams.exists(_._1 == v) || call.outParams.exists(_._2 == v)  || call.actualParams.exists(_._2 == v) => Map()
+        case _ => Map(d->IdEdge()) // unused locals in function ignore proc call
 
       }
-      // currently every global going into each procedure regardless if going to be modified or not - way to check??
   }
 
   def edgesOther(n: CFGPosition)(d: DL): Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]] = {
@@ -99,36 +94,79 @@ trait CopyConstAnalysisFunctions(parameterForm: Boolean) extends ForwardIDEAnaly
 
     n match {
       case LocalAssign(variable, expression, _) =>
-        // shld make this function icl --> figure out if can just put all under one
         d match {
           case Right(_) =>
             val lambdaToLambda : Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]] = Map(d -> IdEdge())
             expression match {
               case LocalVar(_,_,_) | Register(_,_) => lambdaToLambda //not add anything else from lambda
               case BitVecLiteral(value, size) => lambdaToLambda ++ Map(Left(variable)->ConstEdge(valuelattice.bv(BitVecLiteral(value, size))))
+              case BinaryExpr(op, arg1, arg2) => (arg1,  arg2) match {
+                case (BitVecLiteral(value1,size1), BitVecLiteral(value2, size2)) => op match {
+                  //started to add evaluations of simple expressions before assignment e.g. x = 1+2 will be evaluated as x->3 instead of x->Top
+                    case BVADD => lambdaToLambda ++ Map(Left(variable)->ConstEdge(valuelattice.bvadd(valuelattice.bv(BitVecLiteral(value1, size1)), valuelattice.bv(BitVecLiteral(value2, size2)))))
+                    case BVSUB => lambdaToLambda ++ Map(Left(variable)->ConstEdge(valuelattice.bvsub(valuelattice.bv(BitVecLiteral(value1, size1)), valuelattice.bv(BitVecLiteral(value2, size2)))))
+                    case BVMUL => lambdaToLambda ++ Map(Left(variable)->ConstEdge(valuelattice.bvmul(valuelattice.bv(BitVecLiteral(value1, size1)), valuelattice.bv(BitVecLiteral(value2, size2)))))
+                    case _ => lambdaToLambda ++ Map(Left(variable) -> ConstEdge(valuelattice.top))
+
+                  }
+                  case _ => lambdaToLambda ++ Map(Left(variable) -> ConstEdge(valuelattice.top))
+                }
               case _ => lambdaToLambda ++ Map(Left(variable) -> ConstEdge(valuelattice.top))
-            }
+              }
+
 
           case Left(a) =>
             expression match {
-              case LocalVar(_, _, _) | Register(_,_) if expression == a => Map(Left(variable)->IdEdge(), d ->IdEdge()) // idk if this actually checks properly
-              //case LocalVar(_, _, _) | Register(_,_) if expression != a => Map(d->IdEdge())
-              case BitVecLiteral(_, _) => Map() //remove old value
-              case _ => Map(d->IdEdge())
-              //case _ => Map() //ignore other kind of expr, should this be top or nothing <-- or d is identity, var is top??
+              case LocalVar(_, _, _) | Register(_,_) if expression == a => Map(Left(variable)->IdEdge(), d ->IdEdge())
+              case _ => Map() //ignore other kind of expr
 
             }
 
           }
-      case MemoryLoad(variable, _, _, _, _, _)=> //might have ti fix this up
+
+      case MemoryLoad(variable, _, _, _, _, _)=>
         d match {
           case Left(_) => Map(d -> IdEdge())
           case Right(_) => Map(Left(variable) -> ConstEdge(valuelattice.top), d -> IdEdge())
         }
 
+      case exit: Return =>
+        // needed to map abstract states of formal in and out parameters and whether they constant in all calls
+
+        Map(d->IdEdge())
+        d match {
+          case Left(a) =>
+            exit.outParams.toList.foldLeft(Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]]()) {
+              case (m, (retVar, expression)) => expression match
+                case LocalVar(_, _, _) | Register(_, _) if expression == a => m ++ Map(Left(retVar) -> IdEdge())
+                case _ => m ++ Map()
+
+            }
+          case Right(a) =>
+            val lambdaToLambda: Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]] = Map(d -> IdEdge())
+
+            exit.outParams.toList.foldLeft(Map[DL, EdgeFunction[FlatElement[BitVecLiteral]]]()) {
+              case (m, (retVar, expression)) => expression match {
+                case LocalVar(_, _, _) | Register(_, _) => m ++ lambdaToLambda //not add anything else from lambda
+                case BitVecLiteral(value, size) => m ++ lambdaToLambda ++ Map(Left(retVar) -> ConstEdge(valuelattice.bv(BitVecLiteral(value, size))))
+                case _ => m ++ lambdaToLambda ++ Map(Left(retVar) -> ConstEdge(valuelattice.top))
+
+              }
+
+            }
+        }
+
       case _ => Map(d->IdEdge())
     }
+    }
   }
-}
 
+
+/**
+ * Performs copy-constant propagation analysis on a program. Determines the variables with a constant value, thus
+ * providing information for relevant transforms to replace function calls and assignments to variables as assignments
+ * to constants. Note that only information for copy assignments is determined, to allow for distributivity and use of
+ * IDE solver. 'parameterForm' may be unnecessary as this analysis and corresponding transform may only occur after
+ * parameter simplifications have been done.
+ */
 class InterCopyConst(program:Program, parameterForm: Boolean) extends ForwardIDESolver[Variable, FlatElement[BitVecLiteral], ConstantPropagationLattice](program), CopyConstAnalysisFunctions(parameterForm)
