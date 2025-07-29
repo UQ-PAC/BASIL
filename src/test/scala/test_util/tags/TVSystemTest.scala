e8d6ebac8b94d948ac2eeba6d24aa43d4d0b7182
Alistair Michael <alistair.michael@uq.edu.au>
2025-07-29 14:40:43 +1000

    Java

--- src/test/scala/test_util/tags/TVSystemTest.scala
+++
@@ -1,9 +0,0 @@
-package test_util.tags;
-
-import java.lang.annotation.*;
-import org.scalatest.TagAnnotation;
-
-@Inherited
-@TagAnnotation
-@Retention(RetentionPolicy.RUNTIME)
-public @interface TVSystemTest {}
