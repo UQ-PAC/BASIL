7f32fdd417701a253254601a6a8ef7db1c45c259
rina <k@rina.fyi>
2025-08-05 13:24:20 +1000

    Revert "automated scalafmt"
    This reverts commit 8819501a9d6ea7ce35927962a9144adf2767e55b.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -278,7 +278,8 @@ object Main {
     }
 
     Logger.setLevel(LogLevel.INFO, false)
-    if (conf.verbose.value) { Logger.setLevel(LogLevel.DEBUG, true) }
+    if (conf.verbose.value) { Logger.setLevel(LogLevel.DEBUG, true)
+    }
     DebugDumpIRLogger.setLevel(LogLevel.OFF)
     AnalysisResultDotLogger.setLevel(LogLevel.OFF)
     for (v <- conf.verboseLog) {
