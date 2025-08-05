d0eede65f9083c77c04007209853fd0c2575e803
rina <k@rina.fyi>
2025-08-05 14:12:57 +1000

    add ParserTest

---
+++ src/test/scala/ir/ParserTest.scala
@@ -0,0 +1,44 @@
+package ir
+
+import ir.*
+import ir.dsl.*
+import org.scalactic.*
+import org.scalatest.funsuite.AnyFunSuite
+import test_util.{BASILTest, CaptureOutput}
+import translating.PrettyPrinter.*
+import util.{BASILConfig, BoogieGeneratorConfig, ILLoadingConfig, LogLevel, Logger}
+import ir.parsing.ParseBasilIL
+
+import scala.collection.immutable.*
+
+@test_util.tags.UnitTest
+class ParserTest extends AnyFunSuite with CaptureOutput {
+
+  override def withFixture(test: NoArgTest) = {
+    DeepEquality.debug.withValue(true) {
+      super.withFixture(test)
+    }
+  }
+
+  val minimal = """
+prog entry @main_1812;
+
+proc @main_1812 () -> ()
+  { .name = "main"; .address = 0x714 }
+[
+  block %main_basil_return_1 [
+    return ();
+  ]
+];
+  """;
+
+  test("minimal") {
+    ParseBasilIL.loadILString(minimal)
+  }
+
+  test("minimal + declare-fun") {
+    ParseBasilIL.loadILString(minimal + """
+      declare-fun $FPToFixed_bv64_bv1123_bool_bv32_bv1123_bv32 : (bv64, bv1123, bool, bv32, bv1123) -> bv32;
+    """)
+  }
+}
