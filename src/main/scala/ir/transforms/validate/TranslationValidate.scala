d39e8d509d8b6ad2d129f5af78faa1d8d3c1ebb5
rina <k@rina.fyi>
2025-09-16 17:20:50 +1000

    strip body

--- src/main/scala/ir/transforms/validate/TranslationValidate.scala
+++ src/main/scala/ir/transforms/validate/TranslationValidate.scala
@@ -1413,6 +1413,12 @@ object TranslationValidator {
     }
   }
 
+  def stripBody(p: Procedure) = {
+    p.blocks.foreach { b =>
+      b.statements.clear()
+    }
+  }
+
   def forTransform[T](
     transformName: String,
     transform: Program => T,
@@ -1421,6 +1427,7 @@ object TranslationValidator {
     {
       println("writing!!!" + transformName)
       val before = ir.dsl.IRToDSL.convertProgram(p).resolve
+      before.procedures.foreach(stripBody(_))
       util.writeToFile(before.pprint, "before.il")
 
       val beforeprocs = before.nameToProcedure
@@ -1431,12 +1438,15 @@ object TranslationValidator {
       val r = transform(p)
       val inv = invariant(r)
       val after = ir.dsl.IRToDSL.convertProgram(p).resolve
+      after.procedures.foreach(stripBody(_))
       util.writeToFile(after.pprint, "after.il")
       if (transformName == "AssumeCallPreserved") {
         val b = analysis.LoopDetector.identify_loops(before)
         val a = analysis.LoopDetector.identify_loops(after)
         println(b.loops_o.map(_.header.label).toSet -- a.loops_o.map(_.header.label).toSet)
+        println(a.loops_o.map(_.header.label).toSet -- b.loops_o.map(_.header.label).toSet)
         assert(a.loops_o.size == b.loops_o.size, "before and after have differing loop counts?!")
+        throw Exception("boop")
       }
       getValidationSMT(p, tvconf, transformName, before, after, inv)
     }
