f1a9206b5732a19bd9cf859749bca42c119a7cbf
rina <k@rina.fyi>
2025-09-29 15:37:36 +1000

    add RemoveUnreachableBlocks to simplifyCFGValidated
    this lets us exclude read-uninit variables which are unreachable.
    be aware of limitations of the RemoveUnreachableBlocks method.
    
        /**
         * Very coarse transform pass to remove obviously unreachable
         * blocks.
         *
         * This only removes in the most simple cases.
         * Does NOT remove:
         * - mutually-recursive unreachable blocks,
         * - blocks only reachable from unreachable blocks.
         * - any blocks in the presence of indirect calls.
         */
        object RemoveUnreachableBlocks {

--- src/main/scala/ir/transforms/validate/SimplifyPipeline.scala
+++ src/main/scala/ir/transforms/validate/SimplifyPipeline.scala
@@ -12,6 +12,7 @@ import cilvisitor.{visit_proc, visit_prog}
  */
 
 def simplifyCFG(p: Program) = {
+  p.procedures.foreach(transforms.RemoveUnreachableBlocks.apply)
   for (p <- p.procedures) {
     while (transforms.coalesceBlocks(p)) {}
     transforms.removeEmptyBlocks(p)
@@ -280,10 +281,6 @@ def validatedSimplifyPipeline(ctx: IRContext, mode: util.SimplifyMode): (TVJob, 
 
   transforms.applyRPO(p)
   config = guardCleanup(config, p)
-
-  // import translating.PrettyPrinter.pprint
-  // util.writeToFile(ctx.program.pprint, "/tmp/blah.il")
-  assert(ir.invariant.readUninitialised(ctx.program))
 
   counter = ir.transforms.CountGuardStatements()
   val _ = visit_prog(counter, p)
