f3944f83994664c8a33519177761a2f9fd46786d
McArthur-Alford <mcarthur.alford@proton.me>
2026-02-25 09:58:59 +1000

    Back into a compiling state with new memory encoding abstraction, though unused

--- src/main/scala/ir/transforms/memory_encoding/MemoryEncoding.scala
+++ src/main/scala/ir/transforms/memory_encoding/MemoryEncoding.scala
@@ -11,11 +11,12 @@ trait MemoryEncoding {
   // Type used to represent abstract allocation ids
   val allocationType: IRType
   val allocationBType: BType
-  def allocationLiteral(n: int): Expr
+  def allocationLiteral(n: Int): Expr
 
-  val addressType: IRType = BitVecType(64)
-  val addressBType: BType = BitVecBType(64)
-  def addressLiteral(n: int): Expr = BitVecLiteral(n, 64)
+  val addressSize: Int = 64
+  val addressType: IRType = BitVecType(addressSize)
+  val addressBType: BType = BitVecBType(addressSize)
+  def addressLiteral(n: Int): Expr = BitVecLiteral(n, addressSize)
 
   val liveType: IRType = BitVecType(2)
   val liveBType: BType = BitVecBType(2)
@@ -25,10 +26,17 @@ trait MemoryEncoding {
 
   val memEncodingType: IRType = CustomSort("MemEncoding")
   val memEncodingBType: BType = CustomBType("MemEncoding")
+  val memEncodingBDataTypeDecl: BDataTypeDecl = BDataTypeDecl(
+    "MemEncoding",
+    memEncodingBDataTypeBody
+    // List(BDataTypeConstructor("MemEncoding", List(alloc_live_param, alloc_size_param, addr_is_heap_param)))
+  )
+  val memEncodingBDataTypeBody: List[BDataTypeConstructor] = List()
 
   // Necessary boogie declarations for the memory encoding
   def bDeclarations: List[BDeclaration] = {
     List(
+      memEncodingBDataTypeDecl,
       allocSizeFunc,
       allocBaseFunc,
       addrAllocFunc,
@@ -47,64 +55,65 @@ trait MemoryEncoding {
   val pMemEncoding = BVariable("mem_encoding", memEncodingBType, Scope.Parameter)
   val pAddr = BVariable("addr", addressBType, Scope.Parameter)
   val pAlloc = BVariable("alloc", allocationBType, Scope.Parameter)
+  val pLive = BVariable("live", liveBType, Scope.Parameter)
   val pSize = BVariable("size", addressBType, Scope.Parameter)
   val pLb = BVariable("lb", addressBType, Scope.Parameter)
   val pUb = BVariable("ub", addressBType, Scope.Parameter)
+
   val rMemEncoding = BVariable("r", memEncodingBType, Scope.Parameter)
   val rAddr = BVariable("r", addressBType, Scope.Parameter)
   val rAlloc = BVariable("r", allocationBType, Scope.Parameter)
+  val rLive = BVariable("r", liveBType, Scope.Parameter)
   val rBool = BVariable("r", BoolBType, Scope.Parameter)
   val r64 = BVariable("r", BitVecBType(64), Scope.Parameter)
-  val rLive = BVariable("r", liveBType, Scope.Parameter)
 
   // Globals too
   val gMemEncoding = GlobalVar("mem_encoding", CustomSort("MemEncoding"))
 
   // me_alloc_size maps an allocation to its size.
   def allocSizeFunc: BFunction = BFunction("me_alloc_size", List(pMemEncoding, pAlloc), rAddr, allocSizeBody)
-  def allocSizeBody: BExpr
-  require(allocSizeBody.getType == addressBType)
+  def allocSizeBody: Option[BExpr] = None
+  def allocSizeCall(alloc: Expr, me: Expr = gMemEncoding): Expr =
+    FApplyExpr("me_alloc_size", Seq(me, alloc), addressType)
+  def allocSizeBCall(alloc: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
+    BFunctionCall("me_alloc_size", List(me, alloc), addressBType)
 
   // me_alloc_base maps an allocation to its base address.
   def allocBaseFunc: BFunction = BFunction("me_alloc_base", List(pMemEncoding, pAlloc), rAddr, allocBaseBody)
-  def allocBaseBody: BExpr
-  require(allocBaseBody.getType == addressBType)
-
+  def allocBaseBody: Option[BExpr] = None
   def allocBaseCall(alloc: Expr, me: Expr = gMemEncoding): Expr =
-    FApplyExpr("me_alloc_base", Seq(me, alloc), BoolType)
+    FApplyExpr("me_alloc_base", Seq(me, alloc), addressType)
   def allocBaseBCall(alloc: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
-    BFunctionCall("me_alloc_base", List(me, alloc), BoolBType)
+    BFunctionCall("me_alloc_base", List(me, alloc), addressBType)
 
   // me_addr_alloc maps an address to its allocation id.
   // Allocation id must be of type allocationType
-  def addrAllocFunc: BFunction = BFunction("me_addr_alloc", List(pMemEncoding, pAddr), rAlloc, allocBody)
-  def addrAllocBody: BExpr
-  require(allocBody.getType == allocationBType)
-
+  def addrAllocFunc: BFunction = BFunction("me_addr_alloc", List(pMemEncoding, pAddr), rAlloc, addrAllocBody)
+  def addrAllocBody: Option[BExpr] = None
   def addrAllocCall(addr: Expr, me: Expr = gMemEncoding): Expr =
-    FApplyExpr("me_addr_alloc", Seq(me, addr), allocationBType)
+    FApplyExpr("me_addr_alloc", Seq(me, addr), allocationType)
   def addrAllocBCall(addr: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
     BFunctionCall("me_addr_alloc", List(me, addr), allocationBType)
 
   // me_alloc_live maps an allocess to its liveness.
   def allocLiveFunc: BFunction = BFunction("me_alloc_live", List(pMemEncoding, pAlloc), rLive, allocLiveBody)
-  def allocLiveBody: BExpr
-  require(allocLiveBody.getType == liveBType)
+  def allocLiveBody: Option[BExpr] = None
+  def allocLiveCall(alloc: Expr, me: Expr = gMemEncoding): Expr =
+    FApplyExpr("me_alloc_live", Seq(me, alloc), liveType)
+  def allocLiveBCall(alloc: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
+    BFunctionCall("me_alloc_live", List(me, alloc), liveBType)
 
   // me_addr_offset maps an address to its offset within its allocation.
   def addrOffsetFunc: BFunction = BFunction("me_addr_offset", List(pMemEncoding, pAddr), rAddr, addrOffsetBody)
-  def addrOffsetBody: BExpr
-  require(addrOffsetBody.getType == addressBType)
-
+  def addrOffsetBody: Option[BExpr] = None
   def addrOffsetCall(addr: Expr, me: Expr = gMemEncoding): Expr =
-    FApplyExpr("me_addr_offset", Seq(me, addr), BoolType)
+    FApplyExpr("me_addr_offset", Seq(me, addr), addressType)
   def addrOffsetBCall(addr: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
-    BFunctionCall("me_addr_offset", List(me, addr), BoolBType)
+    BFunctionCall("me_addr_offset", List(me, addr), addressBType)
 
   // me_addr_is_heap maps an address to a boolean identifying if it belongs to the heap.
   def addrIsHeapFunc: BFunction = BFunction("me_addr_is_heap", List(pMemEncoding, pAddr), rBool, addrIsHeapBody)
-  def addrIsHeapBody: BExpr
-  require(addrIsHeapBody.getType == BoolBType)
+  def addrIsHeapBody: Option[BExpr] = None
 
   def addrIsHeapCall(addr: Expr, me: Expr = gMemEncoding): Expr =
     FApplyExpr("me_addr_is_heap", Seq(me, addr), BoolType)
@@ -114,22 +123,26 @@ trait MemoryEncoding {
   // me_alloc_size_update returns an updated memory encoding with the size of the given allocation updated.
   def allocSizeUpdateFunc: BFunction =
     BFunction("me_alloc_size_update", List(pMemEncoding, pAlloc, pSize), rMemEncoding, allocSizeUpdateBody)
-  def allocSizeUpdateBody: BExpr
-  require(allocSizeUpdateBody.getType == memEncodingBType)
+  def allocSizeUpdateBody: Option[BExpr] = None
+  def allocSizeUpdateCall(alloc: Expr, size: Expr, me: Expr = gMemEncoding): Expr =
+    FApplyExpr("me_alloc_size_update", Seq(me, alloc, size), memEncodingType)
+  def allocSizeUpdateBCall(alloc: BExpr, size: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
+    BFunctionCall("me_alloc_size_update", List(me, alloc, size), memEncodingBType)
 
   // me_alloc_live_update returns an updated memory encoding with the liveness of the given allocation updated.
   def allocLiveUpdateFunc: BFunction =
     BFunction("me_alloc_live_update", List(pMemEncoding, pAlloc, pLive), rMemEncoding, allocLiveUpdateBody)
-  def allocLiveUpdateBody: BExpr
-  require(allocLiveUpdateBody.getType == memEncodingBType)
+  def allocLiveUpdateBody: Option[BExpr] = None
+  def allocLiveUpdateCall(alloc: Expr, live: Expr, me: Expr = gMemEncoding): Expr =
+    FApplyExpr("me_alloc_live_update", Seq(me, alloc, live), memEncodingType)
+  def allocLiveUpdateBCall(alloc: BExpr, live: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
+    BFunctionCall("me_alloc_live_update", List(me, alloc, live), memEncodingBType)
 
   // me_can_allocate tests whether an address + size pair may be allocated.
   // Useful in simulating out of memory conditions.
   def canAllocateFunc: BFunction =
     BFunction("me_can_allocate", List(pMemEncoding, pAddr, pSize), rBool, canAllocateBody)
-  def canAllocateBody: BExpr
-  require(canAllocateBody.getType == BoolBType)
-
+  def canAllocateBody: Option[BExpr] = None
   def canAllocateCall(addr: Expr, size: Expr, me: Expr = gMemEncoding): Expr =
     FApplyExpr("me_can_allocate", Seq(me, addr, size), BoolType)
   def canAllocateBCall(addr: BExpr, size: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
@@ -139,18 +152,31 @@ trait MemoryEncoding {
   // allocation at pAddr with size pSize.
   def allocateFunc: BFunction =
     BFunction("me_allocate", List(pMemEncoding, pAddr, pSize), rMemEncoding, allocateBody)
-  def allocateBody: BExpr
-  require(allocateBody.getType == memEncodingBType)
-
-  def AllocateCall(addr: Expr, size: Expr, me: Expr = gMemEncoding): Expr =
-    FApplyExpr("me_allocate", Seq(me, addr, size), me.memEncodingType)
-  def AllocateBCall(addr: BExpr, size: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
-    BFunctionCall("me_allocate", List(me, addr, size), me.memEncodingBType)
+  def allocateBody: Option[BExpr] = None
+  def allocateCall(addr: Expr, size: Expr, me: Expr = gMemEncoding): Expr =
+    FApplyExpr("me_allocate", Seq(me, addr, size), memEncodingType)
+  def allocateBCall(addr: BExpr, size: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
+    BFunctionCall("me_allocate", List(me, addr, size), memEncodingBType)
 
   // me_init_heap makes all assertions to initialize the heap within the given lb and ub addresses
-  def initHeapFunc: BFunction = BFunction("me_init_heap", List(pMemEncoding, pLb, pUb))
-  def initHeapBody: BExpr
-  require(initHeapBody.getType == BoolBType)
+  def initHeapFunc: BFunction = BFunction("me_init_heap", List(pMemEncoding, pLb, pUb), rBool, initHeapBody)
+  def initHeapBody: Option[BExpr] = None
+  def initHeapCall(lb: Expr, ub: Expr, me: Expr = gMemEncoding): Expr =
+    FApplyExpr("me_init_heap", Seq(me, lb, ub), BoolType)
+  def initHeapBCall(lb: BExpr, ub: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
+    BFunctionCall("me_init_heap", List(me, lb, ub), BoolBType)
+
+  // me_valid_access ensures accessing an address + size is memory safe:
+  // - An allocation exists at the address (or teh address is not on the heap)
+  // - The accessed region is fully within bounds of the allocation
+  // - The allocation is live
+  def validAccessFunc: BFunction =
+    BFunction("me_valid_access", List(pMemEncoding, pAddr, pSize), rBool, validAccessBody)
+  def validAccessBody: Option[BExpr] = None
+  def validAccessCall(addr: Expr, size: Expr, me: Expr = gMemEncoding): Expr =
+    FApplyExpr("me_valid_access", Seq(me, addr, size), BoolType)
+  def validAccessBCall(addr: BExpr, size: BExpr, me: BExpr = gMemEncoding.toBoogie): BExpr =
+    BFunctionCall("me_valid_access", List(me, addr, size), BoolBType)
 }
 
-class SplitEncoding(override val repr: MemoryEncodingRepresentation, override val simplify: Boolean) {}
+// class SplitEncoding(override val repr: MemoryEncodingRepresentation, override val simplify: Boolean) {}
--- src/main/scala/ir/transforms/memory_encoding/MemorySpecification.scala
+++ src/main/scala/ir/transforms/memory_encoding/MemorySpecification.scala
@@ -3,6 +3,7 @@ package ir.transforms.memoryEncoding
 import util.MemoryEncodingRepresentation
 import boogie.*
 import ir.*
+import ir.cilvisitor.*
 
 case class MemoryEncodingTransform(
   val specs: List[MemorySpecification],
@@ -34,16 +35,16 @@ trait MemorySpecificationCore extends MemorySpecification {
   def specifyMain(p: Procedure, h: Helpers) = {}
 
   def specifyMalloc(p: Procedure, h: Helpers) = {
-    p.modifies ++= Set(h.me.memEncoding)
-    if h.simplify then p.modifies ++= Set(h.preR(0))
+    p.modifies ++= Set(h.me.gMemEncoding)
+    if h.me.simplify then p.modifies ++= Set(Register("R0", h.me.addressSize))
 
     p.requires ++= List(BinaryBExpr(EQ, h.preR(0).toGamma, TrueBLiteral))
 
     p.requiresExpr ++= List(
-      QuantifierExpr(QuantifierSort.exists, LambdaExpr(List(h.i), h.canAllocate(h.i, h.preR(0), map=h.me.memEncoding)))
+      QuantifierExpr(QuantifierSort.exists, LambdaExpr(List(h.i), h.me.canAllocateCall(h.i, h.preR(0), me=h.me.gMemEncoding)))
     )
 
-    p.ensures ++= List(BinaryBExpr(EQ, h.r(0).toGamma, TrueBLiteral))
+    p.ensures ++= List(BinaryBExpr(EQ, h.postR(0).toGamma, TrueBLiteral))
 
     p.ensuresExpr ++= List(
       // BinaryExpr(BVUGT, BinaryExpr(BVADD, r(0), old_r(0)), r(0)),
@@ -58,15 +59,15 @@ trait MemorySpecificationCore extends MemorySpecification {
       // addrIsHeap(r(0), map = OldExpr(mem_encoding)),
       
       // We are allowed to allocate r0 with size old(r0)
-      h.me.canAllocateCall(h.r(0), h.oldR(0), map=OldExpr(h.me.memEncoding)),
+      h.me.canAllocateCall(h.postR(0), h.oldR(0), me=OldExpr(h.me.gMemEncoding)),
       // Ensure offset of return address r(0) is 0
-      BinaryExpr(EQ, h.me.addrOffsetCall(h.r(0)), h.me.addrLiteral(0)),
+      BinaryExpr(EQ, h.me.addrOffsetCall(h.postR(0)), h.me.addressLiteral(0)),
       // Ensure base of return allocation is r(0)
-      BinaryExpr(EQ, h.me.allocBaseCall(h.me.addrAllocCall(h.r(0))), h.r(0)),
+      BinaryExpr(EQ, h.me.allocBaseCall(h.me.addrAllocCall(h.postR(0))), h.postR(0)),
       // Update the memory encoding:
-      BinaryExpr(EQ, h.me.memEncoding,
+      BinaryExpr(EQ, h.me.gMemEncoding,
         // To allocate old(r0) space at r(0) address
-        h.allocate(h.r(0), h.oldR(0), OldExpr(h.me.memEncoding))
+        h.me.allocateCall(h.postR(0), h.oldR(0), OldExpr(h.me.gMemEncoding))
       ),
     )
   }
@@ -89,23 +90,21 @@ trait MemorySpecificationCore extends MemorySpecification {
 // }
 
 private case class Helpers(val me: MemoryEncoding) {
-  def me: MemoryEncoding = me
-
   // For use in postconditions. rn, or rn_out if simplify
-  def postR(n: Int) = if me.simplify then LocalVar(s"R${n}_out", BitVecType(64)) else Register(s"R$n", 64)
+  def postR(n: Int) = if me.simplify then LocalVar(s"R${n}_out", BitVecType(me.addressSize)) else Register(s"R$n", me.addressSize)
 
   // For use in postconditions (obviously). old(rn), or rn_in if simplify
-  def oldR(n: Int) = if me.simplify then LocalVar(s"R${n}_in", BitVecType(64)) else OldExpr(Register(s"R$n", 64))
+  def oldR(n: Int) = if me.simplify then LocalVar(s"R${n}_in", BitVecType(me.addressSize)) else OldExpr(Register(s"R$n", me.addressSize))
   def oldGammaR(n: Int) =
-    if me.simplify then LocalVar(s"R${n}_in", BitVecType(64)).toGamma else Old(Register(s"R$n", 64).toGamma)
+    if me.simplify then LocalVar(s"R${n}_in", BitVecType(me.addressSize)).toGamma else Old(Register(s"R$n", me.addressSize).toGamma)
 
   // For use in preconditions. rn, rn_in if simplify
-  def preR(n: Int) = if me.simplify then LocalVar(s"R${n}_in", BitVecType(64)) else Register(s"R$n", 64)
+  def preR(n: Int) = if me.simplify then LocalVar(s"R${n}_in", BitVecType(me.addressSize)) else Register(s"R$n", me.addressSize)
 
   // (always try to use mem, unsplit/split should rarely be necessary)
   val unsplitMem = SharedMemory("mem", 64, 8)
   val splitMem = SharedMemorySplit("split_mem", 64, 8)
-  val mem = (if me.repr == MemoryEncodingRepresentation.Split(splitMem = true) then unsplit_mem else split_mem)
+  val mem = (if me.repr == MemoryEncodingRepresentation.Split(splitMem = true) then unsplitMem  else splitMem)
 
   val i = LocalVar("i", BitVecType(64))
 }
