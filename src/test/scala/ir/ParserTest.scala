b99487bb68d1818460337234196482e8d61a8e63
rina <k@rina.fyi>
2026-05-16 15:53:51 +1000

    add test of pprint. sbasil doesn't do anything with Decl_UninterpFun lol

--- src/test/scala/ir/ParserTest.scala
+++ src/test/scala/ir/ParserTest.scala
@@ -3,6 +3,7 @@ package ir
 import ir.parsing.ParseBasilIL
 import org.scalatest.funsuite.AnyFunSuite
 import test_util.CaptureOutput
+import translating.PrettyPrinter.pprint
 
 import scala.collection.immutable.*
 
@@ -32,8 +33,22 @@ proc @main_1812 () -> ()
   }
 
   test("minimal + uninterp fun") {
-    ParseBasilIL.loadILString(minimal + """
+    val p = ParseBasilIL.loadILString(minimal + """
       val $FPToFixed_bv64_bv1123_bool_bv32_bv1123_bv32 : bv64 -> bv1123 -> bool -> bv32 -> bv1123 -> bv32;
     """)
+
+    assertResult {
+      """prog entry @main_1812;
+
+proc @main_1812 () -> ()
+  { .name = "main"; .address = 0x714 }
+[
+  block %main_basil_return_1 [
+    return ();
+  ]
+];"""
+    } {
+      p.program.pprint.trim
+    }
   }
 }
